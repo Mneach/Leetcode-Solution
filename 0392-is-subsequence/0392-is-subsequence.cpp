30bb01114c1fad23655384116649f2b4266e15d4
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-11 17:54:00 +0700

    Time: 4 ms (45.47%), Space: 6.4 MB (71.45%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        
+        int currentIndex = 0;
+        
+        for(int i = 0; i < t.length(); i++){
+            if(s[currentIndex] == t[i]) currentIndex++;
+        }
+        
+        return s.length() == currentIndex;
+    }
+};
