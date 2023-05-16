2dde773bbb12e007a349206637da23ccecb58da2
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-16 23:09:21 +0700

    Time: 13 ms (32.69%), Space: 15.1 MB (93.84%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.cpp
+++ 1528-shuffle-string/1528-shuffle-string.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string restoreString(string s, vector<int>& indices) {
         
-        string answer = s;
+        string answer(s.length(), ' ');
         
         for(int i = 0; i < s.length(); i++){
             answer[indices[i]] = s[i];
