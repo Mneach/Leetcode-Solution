2d914d887336b2cc51e853a2b1c9d7d5d0446f18
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-18 20:18:36 +0700

    Time: 0 ms (100.00%), Space: 7.2 MB (59.45%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -1,11 +1,7 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-       
         if(str1 + str2 != str2 + str1) return "";
-        
-        int gcdLength = gcd(str1.size(), str2.size());
-        
-        return str1.substr(0 , gcdLength);
+        else return str1.substr(0 , gcd(str1.size(), str2.size()));
     }
 };
