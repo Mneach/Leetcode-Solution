3d5c2429c7f340d91cb2e2d1961f41ed2c727824
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-19 09:27:44 +0700

    Time: 241 ms (78.51%), Space: 69.4 MB (28.07%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.cpp
@@ -3,32 +3,33 @@ public:
     
     int result = 0;
     
-    int maxMoves(vector<vector<int>>& grid, int currentRow, int currentColumn, int maxRow, int maxColumn, int prev, vector<vector<int>> & dp){
+    int maxMoves(vector<vector<int>>& grid, int i, int j, int m, int n,int prev, vector<vector<int>> & dp){
+       
+        if(i < 0 || j < 0 || i >= m || j >=  n) return 0;
         
-        if(currentRow < 0 || currentColumn < 0 || currentRow >= maxRow || currentColumn >=  maxColumn) return 0;
+        int currentCell = grid[i][j];
         
-        int currentCell = grid[currentRow][currentColumn];
         if(prev >= currentCell) return 0;
         
-        if(dp[currentRow][currentColumn] != -1) return dp[currentRow][currentColumn];
+        if(dp[i][j] != -1) return dp[i][j];
         
-        int moveTopRight = maxMoves(grid, currentRow - 1, currentColumn + 1, maxRow, maxColumn, currentCell, dp);
-        int moveRight = maxMoves(grid, currentRow, currentColumn + 1, maxRow, maxColumn, currentCell, dp);
-        int moveBottomRight = maxMoves(grid, currentRow  +  1, currentColumn + 1, maxRow, maxColumn, currentCell, dp);
+        int moveTopRight = maxMoves(grid, i - 1, j + 1, m, n,  currentCell, dp);
+        int moveRight = maxMoves(grid, i, j + 1, m, n, currentCell, dp);
+        int moveBottomRight = maxMoves(grid, i  +  1, j + 1, m, n, currentCell, dp);
         
-        return dp[currentRow][currentColumn] = max({moveTopRight, moveRight, moveBottomRight}) + 1;
+        return dp[i][j] = max({moveTopRight, moveRight, moveBottomRight}) + 1;
     }
     
     int maxMoves(vector<vector<int>>& grid) {
         
-        int maxRow = grid.size();
-        int maxColumn = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
         int answer = 0;
         
-        vector<vector<int>> dp(maxRow, vector<int>(maxColumn , -1));
+        vector<vector<int>> dp(m, vector<int>(n , -1));
         
         for(int i = 0; i < grid.size(); i++){
-            int temp = maxMoves(grid, i , 0, maxRow, maxColumn, 0, dp);
+            int temp = maxMoves(grid, i , 0, m, n, 0, dp);
             answer = max(temp , answer);
         }
         
