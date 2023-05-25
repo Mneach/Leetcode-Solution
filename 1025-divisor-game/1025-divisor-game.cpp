fca8c456918c0f3b1c27d29285e9f5d505a0995f
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-25 13:55:37 +0700

    Time: 0 ms (100.00%), Space: 5.9 MB (83.05%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool divisorGame(int n) {
-        
-        if(n == 1) return false;
-        else if(n % 2 == 0) return true;
-        else return false;
+        return n % 2 == 0;
     }
 };
