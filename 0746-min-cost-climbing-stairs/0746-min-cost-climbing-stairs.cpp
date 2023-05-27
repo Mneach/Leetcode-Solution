8d24ceaa0aa1c835f7614208bdb7f267e58af7c7
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-27 13:25:32 +0700

    Time: 0 ms (100.00%), Space: 13.8 MB (49.54%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     
     int minCostClimbingStairs(vector<int>& cost) {
         
-        vector<int> dp(cost.size() + 1);
+        vector<int> dp(cost.size());
         dp[0] = cost[0];
         dp[1] = cost[1];
         
@@ -11,8 +11,7 @@ public:
             dp[i] = cost[i] + min(dp[i - 1] , dp[i - 2]);
         }
         
-        dp[cost.size()] = min(dp[cost.size() - 1] , dp[cost.size() - 2]);
         
-        return dp[cost.size()];
+        return min(dp[cost.size() - 1] , dp[cost.size() - 2]);
     }
 };
