420bc3352469b7eb2737b1885912de0687f5439c
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-27 12:54:46 +0700

    Time: 0 ms (100.00%), Space: 13.6 MB (77.63%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     
-    int climb(int currentPosition, vector<int>&cost, vector<int>& dp){
-        if(currentPosition == cost.size()) return 0;
-        else if (currentPosition == cost.size() - 1) return cost[currentPosition];
-        else if (dp[currentPosition] != -1){
-            if(dp[currentPosition] < cost[currentPosition]) dp[currentPosition] = cost[currentPosition];
-            
-            return dp[currentPosition];
-        }
-        
-        dp[currentPosition] = cost[currentPosition] + min(climb(currentPosition + 1, cost, dp), climb(currentPosition + 2 , cost, dp));
-        return dp[currentPosition];
-    }
-    
     int minCostClimbingStairs(vector<int>& cost) {
         
-        vector<int> dp(cost.size() , -1);
-            
-        int answer = min(climb(0, cost, dp), climb(1, cost, dp));
+        vector<int> dp(cost.size() + 1);
+        dp[0] = cost[0];
+        dp[1] = cost[1];
         
-        return answer;
+        for(int i = 2; i < cost.size(); i++){
+            dp[i] = cost[i] + min(dp[i - 1] , dp[i - 2]);
+        }
+        
+        dp[cost.size()] = min(dp[cost.size() - 1] , dp[cost.size() - 2]);
+        
+        return dp[cost.size()];
     }
 };
