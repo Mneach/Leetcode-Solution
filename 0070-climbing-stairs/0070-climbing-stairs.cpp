7d78ad2f359db3d0c62b0ceb5d8a9467e8120a52
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-05-29 11:38:08 +0700

    Time: 0 ms (100.00%), Space: 6.3 MB (17.67%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int climbStairs(int n) {
-        
+    
+    int answer(int n , vector<int>& dp){
         if(n <= 2) return n;
         
-        int dp[n];
-        dp[0] = 1;
-        dp[1] = 2;
+        if(dp[n] != -1) return dp[n];
         
-        for(int i = 2; i < n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
-        }
+        dp[n] = answer(n - 1 , dp) + answer(n - 2, dp);
+        return dp[n];
+    }
+    
+    int climbStairs(int n) {
         
-        return dp[n - 1];
+        vector<int> dp(n + 1, -1);
+        
+        return answer(n , dp);
     }
 };
