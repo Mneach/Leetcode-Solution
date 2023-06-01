c955e1fcaa08e1ad9110e0048552d0265314d834
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-06-01 23:38:02 +0700

    Time: 0 ms (100.00%), Space: 5.9 MB (65.30%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minimumSum(int num) {
+        string s = to_string(num);
+        
+        sort(s.begin(), s.end());
+        
+        int answer = (( (s[0] - '0') * 10) + s[2] - '0') + (( (s[1] - '0') * 10) + s[3] - '0');
+        
+        return answer;
+    }
+};
