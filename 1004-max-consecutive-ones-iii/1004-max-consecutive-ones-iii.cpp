bdb08ade190ca526a610db163404f120e458d547
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-06-17 04:58:43 +0700

    Time: 81 ms (17.48%), Space: 55.4 MB (91.37%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -4,12 +4,11 @@ public:
         int answer = 0;
         
         int l = 0;
-        int totalZero = 0;
         for(int r = 0; r < nums.size(); r++){
-            if(nums[r] == 0) totalZero++;
+            if(nums[r] == 0) k--;
             
-            while(totalZero > k){
-                if(nums[l] == 0) totalZero--;
+            while(k < 0){
+                if(nums[l] == 0) k++;
                 l++;
             }
                 
