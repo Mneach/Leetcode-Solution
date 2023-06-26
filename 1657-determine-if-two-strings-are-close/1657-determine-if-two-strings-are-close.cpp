af6f1c9f7ef7d0f43dd5e6d057051962ee8a82c7
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-06-26 11:05:38 +0700

    Time: 181 ms (30.16%), Space: 20.7 MB (55.86%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        if(word1.size() != word2.size()) return false;
-     
-        map<char, int> hashmapWord1;
-        map<char, int> hashmapWord2;
         
-        for(int i = 0; i < word1.length(); i++){
-            hashmapWord1[word1[i]]++;
-            hashmapWord2[word2[i]]++;
+        if(word1.length() != word2.length()) return false;
+        
+        vector<int> arr1(26, 0);
+        vector<int> arr2(26, 0);
+        
+        set<char> string1, string2;
+        
+        for(int i = 0; i < word1.size(); i++) {
+            arr1[word1[i] - 'a']++;
+            string1.insert(word1[i]);
+        }
+        for(int i = 0; i < word2.size(); i++) {
+            arr2[word2[i] - 'a']++;
+            string2.insert(word2[i]);
         }
         
-        for(auto x : hashmapWord1){
-            if(hashmapWord2.find(x.first) != hashmapWord2.end()){
-                if(x.second == hashmapWord2[x.first]) continue;
-                else {
-                    bool check = false;
-                    int find = hashmapWord2[x.first];
-                    for(auto y : hashmapWord1){
-                        if(y.first > x.first){
-                            if(find == y.second) {
-                                int temp = x.second;
-                                hashmapWord1[x.first] = y.second;
-                                hashmapWord1[y.first] = temp;
-                                cout << x.first << " " << x.second << endl;
-                                cout << y.first << " " << y.second << endl;
-                                check = true;
-                                break;
-                            }
-                        }
-                    }
-                    
-                    if(check == false) return false;
-                }
-            }else{
-                return false;
-            }
-        }
+        sort(arr1.begin(), arr1.end());
+        sort(arr2.begin(), arr2.end());
         
-        return true;
+        return arr1 == arr2 && string1 == string2;        
     }
 };
