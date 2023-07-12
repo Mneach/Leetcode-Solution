d933a0a6d0cd1a9b9111e3ed345dc6ef59ee01ab
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-07-12 12:59:48 +0700

    Time: 4 ms (87.27%), Space: 17.2 MB (10.68%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,14 +1,30 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-       sort(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
+        vector<int> answer; 
+        int start = 0;
+        int end = nums.size() - 1;
+        binary_search(nums, answer, start, end, target);
         
-       vector<int> result;
+        return answer;
+    }
+    
+    void binary_search(vector<int> nums, vector<int>& answer, int start, int end, int target){
         
-       for(int i = 0; i < nums.size(); i++){
-           if(nums[i] == target) result.push_back(i);
-       }
+        if(start > end) return;
         
-        return result;
+        int mid = (start + end) / 2;
+        
+        if(nums[mid] == target){
+           binary_search(nums, answer, start, mid - 1, target);
+           answer.push_back(mid);
+           binary_search(nums, answer, mid + 1, end, target);
+        }else if(nums[mid] > target){
+            binary_search(nums, answer, start, mid - 1, target); 
+        }else if(nums[mid] < target){
+            binary_search(nums, answer, mid + 1, end, target);
+        }
+        
     }
 };
