bb79c12e7ae1f68bc025cdf02b96ac905a53d6f7
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-07-19 16:00:18 +0700

    Time: 0 ms (100.00%), Space: 6.6 MB (84.78%) - LeetHub

--- 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     string removeOccurrences(string s, string part) {
-       
-        while(true){
-            if(s.find(part) == string::npos) break;
-            else {
-                int start = s.find(part);
-                
-                s.erase(start, part.size());
-            }
+        
+        size_t position = s.find(part);
+        
+        while(position != string::npos){
+            s.erase(position, part.size());
+            position = s.find(part);
         }
         
         return s;
