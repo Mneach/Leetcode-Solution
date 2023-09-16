58c40b1c94840c43e92ff7893734f5901b21a68f
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-09-16 23:21:21 +0700

    Time: 2 ms (75.45%), Space: 6.3 MB (50.62%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,42 +1,15 @@
 class Solution {
 public:
     
-    void recursive(int n, int k, int currentPosition, vector<int> &arr){
+    int recursive(int n, int k){
         
-        if(n == 1) return;
-       
-        int temp = 0;
-        while(true){
-            
-            if(arr[currentPosition] == 1){
-                temp++;
-            }
-            
-            if(temp == k) break;
-            
-            currentPosition = (currentPosition + 1) % arr.size();
-        }
-        
-        arr[currentPosition] = 0;
-        
-        recursive(n - 1, k , (currentPosition + 1) % arr.size(), arr);
+        if(n == 1) return 0;
+        return (recursive(n - 1, k) + k) % n;
     }
     
     int findTheWinner(int n, int k) {
         
-        vector<int> arr(n, 1);
-        
-        recursive(n, k, 0, arr);
-        
-        int answer = 1;
-        
-        for(int i = 0; i < arr.size(); i++){
-            if(arr[i] == 1){
-                answer = i;
-                break;
-            }
-        }
-        
+        int answer = recursive(n , k);
         return answer + 1;
     }
 };
