a7f108a41bca1e07ee21f413bfb61cd21b50f90a
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-10-27 20:37:23 +0700

    Time: 3 ms (96.17%), Space: 14.2 MB (64.21%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     
-    void floodFill(int column, int row, vector<vector<int>>& image, int color, int searchColor, vector<vector<bool>>& visit){
+    void floodFill(int column, int row, vector<vector<int>>& image, int color, int searchColor){
         
         if(row < 0 || column < 0) return;
         else if(row == image.size() || column == image[0].size()) return;
         else if(image[row][column] != searchColor) return;
-        else if(visit[row][column]) return;
+        else if(image[row][column] == color) return;
         
         image[row][column] = color;
-        visit[row][column] = true;
         
-        floodFill(column - 1, row, image, color, searchColor, visit); // move top 
-        floodFill(column + 1, row, image, color, searchColor, visit); // move bottom 
-        floodFill(column, row + 1, image, color, searchColor, visit); // move right 
-        floodFill(column, row - 1, image, color, searchColor, visit); // move left 
+        floodFill(column - 1, row, image, color, searchColor); // move top 
+        floodFill(column + 1, row, image, color, searchColor); // move bottom 
+        floodFill(column, row + 1, image, color, searchColor); // move right 
+        floodFill(column, row - 1, image, color, searchColor); // move left 
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
-        vector<vector<bool>> visit(image.size(), vector<bool>(image[0].size(), false)); 
-        floodFill(sc, sr, image, color, image[sr][sc], visit);
+        floodFill(sc, sr, image, color, image[sr][sc]);
         
         return image;
     }
