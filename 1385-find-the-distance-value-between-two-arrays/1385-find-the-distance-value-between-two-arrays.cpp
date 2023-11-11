d8f3405cd670199cbd6277846bf4e0f3db57b9e9
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-11-11 09:09:08 +0700

    Time: 12 ms (42.40%), Space: 13.4 MB (58.78%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -6,16 +6,31 @@ public:
         
         
         int answer = 0;
+        sort(arr2.begin(), arr2.end());
         for(int i = 0; i < arr1.size(); i++){
+            int left = 0;
+            int right = arr2.size() - 1;
+            
             bool valid = true;
-            for(int j = 0; j < arr2.size(); j++){
-                if(abs(arr1[i] - arr2[j]) <= d){
+            
+            int A = arr1[i];
+            while(left <= right){
+                int mid = (left + right) / 2;
+                int B = arr2[mid];
+                
+                if(A - d <= B && A + d >= B){
                     valid = false;
                     break;
-                } 
+                }else if(A - d > B){
+                    left = mid + 1;
+                }else if(A + d < B){
+                    right = mid - 1;
+                }
+                
             }
-            
-            if(valid) {
+                
+            //cout << endl;
+            if(valid){
                 answer++;
             }
         }
