016ab279d0585802bc20baf2a2539f6ba4a5bdb1
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-11-27 22:18:00 +0700

    Time: 4 ms (76.56%), Space: 6.7 MB (93.80%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string finalString(string s) {
+        
+        string result = "";
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] == 'i'){
+                reverse(result.begin(), result.end());
+            }else{
+                result += s[i];
+            }
+        }
+        
+        return result;
+    }
+};
