7a6d353a2dd250a8ef43a035ddda7a49cad97244
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-12-03 11:52:14 +0700

    Time: 11 ms (20.00%), Space: 34.3 MB (40.00%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void bsta(vector<int> &answer, vector<int> mountain, int left, int right){
+    void binary_search(vector<int> &answer, vector<int> mountain, int left, int right){
         
         if(left <= right){
             int mid = (left + right) / 2;
@@ -10,15 +10,15 @@ public:
                 answer.push_back(mid);
             }
             
-            bsta(answer, mountain, left , mid - 1);
-            bsta(answer, mountain, mid + 1, right);
+            binary_search(answer, mountain, left , mid - 1);
+            binary_search(answer, mountain, mid + 1, right);
         }
     }
     
     vector<int> findPeaks(vector<int>& mountain) {
         
         vector<int> answer;
-        bsta(answer, mountain, 1, mountain.size() - 2);
+        binary_search(answer, mountain, 1, mountain.size() - 2);
         
         return answer;
     }
