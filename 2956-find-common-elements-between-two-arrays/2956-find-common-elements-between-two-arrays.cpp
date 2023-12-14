baeb2c3d1cf959b574fa3d73ce2f35b3967e9c68
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2023-12-14 20:55:37 +0700

    Time: 25 ms (95.12%), Space: 60.9 MB (78.22%) - LeetHub

---
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
+        
+        int answer1 = 0;
+        int answer2 = 0;
+        
+        for(int i = 0; i < nums1.size(); i++){
+            
+            if(find(nums2.begin(), nums2.end(), nums1[i]) != nums2.end()){
+                answer1++;
+            }
+        }
+        
+        for(int i = 0; i < nums2.size(); i++){
+            
+            if(find(nums1.begin(), nums1.end(), nums2[i]) != nums1.end()){
+                answer2++;
+            }
+        }
+        
+        return {answer1, answer2};
+    }
+};
