8127c3383ac026c2b4d6c7908c12e29101cdf170
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-01-13 20:48:38 +0700

    Time: 0 ms (100.00%), Space: 6.6 MB (62.43%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -2,58 +2,24 @@ class Solution {
 public:
     int numDecodings(string s) {
         
-        if(s.length() == 1 && s[0] == '0') return 0;
-        
-        long long int dp[101];
+        vector<int> dp(s.length() + 1, 0);
         
         dp[0] = 1;
-        dp[1] = 1;
-        
-        for(int i = 2; i < 92; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+        dp[1] = s[0] == '0' ? 0 : 1;
+            
+        for(int i = 2; i < dp.size(); i++){
+            
+            if(s[i - 1] != '0'){
+                dp[i] = dp[i - 1];
+            }
+            
+            string currentNumber = s.substr(i - 2, 2);
+            if(currentNumber >= "10" && currentNumber <= "26"){
+                dp[i] += dp[i - 2];
+            }
+            
         }
         
-        int answer = 1;
-        
-        int totalValid = 1;
-        for(int i = 1; i < s.length(); i++){
-            
-            if(s[i] == '0' && s[i - 1] == '0'){
-                answer = 0;
-                break;
-            }
-            
-            if(s[i - 1] == '0' && i == 1){
-                answer = 0;
-                totalValid = 0;
-                continue;
-            }else if(s[i - 1] == '0'){
-                totalValid = 1;
-                continue;
-            }
-            
-            
-            string currentNumber = s.substr(i - 1, 2);
-            
-            if(currentNumber > "26" && currentNumber[1] == '0'){
-                answer = 0;
-                break;
-            }
-            
-            if(currentNumber > "26" || s[i] == '0'){
-                if(s[i] == '0') totalValid--;
-                
-                answer *= dp[totalValid];
-                totalValid = 1;
-            }else{
-                totalValid++;
-            }
-        }
-        
-        if(totalValid > 0){
-            answer *= dp[totalValid];
-        }
-        
-        return answer;
+        return dp[s.length()];
     }
 };
