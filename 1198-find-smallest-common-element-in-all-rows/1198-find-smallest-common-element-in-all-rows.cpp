720e7c5b58b01ad64d6baeb0571b0ceba026585a
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-04-21 16:37:06 +0700

    Time: 44 ms (91.86%), Space: 26.5 MB (81.40%) - LeetHub

--- 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.cpp
+++ 1198-find-smallest-common-element-in-all-rows/1198-find-smallest-common-element-in-all-rows.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     int smallestCommonElement(vector<vector<int>>& mat) {
-        map<int,int> mp;
+        vector<int> pos(mat.size());
         
-        for(int i = 0; i < mat.size(); i++){
-            for(int j = 0; j < mat[i].size(); j++){
-                mp[mat[i][j]]++;
+        for(int j = 0; j < mat[0].size(); j++){
+            bool found = true;
+            
+            for(int i = 1; i < mat.size() && found; i++){
+                pos[i] = lower_bound(mat[i].begin() + pos[i], mat[i].end(), mat[0][j]) - mat[i].begin();
+                
+                if(pos[i] >= mat[0].size()) return -1;
+                
+                found = mat[i][pos[i]] == mat[0][j];
             }
-        }
-        
-        for(auto data : mp){
-            if(data.second == mat.size()){
-                return data.first;
+            
+            if(found){
+                return mat[0][j];
             }
         }
         
