29ca852c51dbd936c4b751d0a8946266f45dfe97
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-05-11 18:23:55 +0700

    Time: 30 ms (59.63%), Space: 34.2 MB (57.89%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,22 +2,54 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         
+        if(arr.size() == k){
+            return arr;
+        }
+        
         vector<int> answer;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
         
-        for(int i = 0; i < arr.size(); i++){
-            pq.push(make_pair(abs(arr[i] - x), i));
+        int left = 0;
+        int right = arr.size() - 1;
+        int mid = 0;
+        
+        // find closed elements
+        while(left < right){
+            mid = left + (right - left) / 2;
+            
+            if(arr[mid] >= x){
+                right = mid;
+            }else if(arr[mid] < x){
+                left = mid + 1;
+            }
         }
         
-        while(pq.empty() == false && k > 0){
-            pair<int,int> data = pq.top();
-            answer.push_back(arr[data.second]);
-            pq.pop();
-            k--;
+        right = left;
+        left = right - 1;
+        
+        // using two pointer to get the range of answers
+        while(right - left - 1 < k){
+            // cout << left << " " << right <<  endl;
+            if(left >= 0 && right < arr.size()){
+                
+                if(abs(arr[left] - x) <= abs(arr[right] - x)){
+                    left--;
+                }else{
+                    right++;
+                }
+            }else if(left < 0 && right < arr.size()){
+                right++;
+            }else if(left > 0 && right >= arr.size()){
+                left--;
+            }
         }
         
-        sort(answer.begin(), answer.end());
+        cout << left << " " << right << endl;
         
-        return answer; 
+        // copy the answers
+        for(int i = left + 1; i < right; i++){
+            answer.push_back(arr[i]);
+        }
+        
+        return answer;
     }
 };
