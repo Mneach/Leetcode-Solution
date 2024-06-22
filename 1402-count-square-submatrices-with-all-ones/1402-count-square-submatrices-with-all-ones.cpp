47496ef3fbb7aa485c651d2b02f61df5589feb80
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-06-22 10:57:06 +0700

    Time: 50 ms (32.67%), Space: 26.2 MB (85.34%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -2,17 +2,28 @@ class Solution {
 public:
     int countSquares(vector<vector<int>>& matrix) {
         
-        int answer = 0;
-        for(int i = 0; i < matrix.size(); i++){
-            for(int j = 0; j < matrix[0].size(); j++){
-                if(i > 0 && j > 0 && matrix[i][j] >= 1){
-                    matrix[i][j] = min(matrix[i - 1][j - 1], min(matrix[i - 1][j], matrix[i][j - 1])) + 1;
-                }
+if (matrix.empty()) return 0;
 
-                answer += matrix[i][j];
-            }
-        }
+  int m = matrix.size();
+  int n = matrix[0].size();
 
-        return answer;
+  int totalSquares = 0;
+
+  for (int i = 0; i < m; i++) {
+    for (int j = 0; j < n; j++) {
+	if (matrix[i][j] == 0) continue;
+
+      int left = (i == 0) ? 0 : matrix[i-1][j];
+      int top = (j == 0) ? 0 :  matrix[i][j-1];
+      int lefttop = (i == 0 || j == 0) ? 0 : matrix[i-1][j-1];
+
+      matrix[i][j] = min(min(left, top), lefttop) + 1;
+
+      totalSquares += matrix[i][j];
+    }
+  }
+
+  return totalSquares;
+
     }
 };
