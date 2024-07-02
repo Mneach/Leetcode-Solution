32f9b43ea71eeb6f8b3d1b640467360bd8564078
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-07-02 20:00:23 +0700

    Time: 2 ms (93.54%), Space: 8.3 MB (96.48%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -2,34 +2,16 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
 
-        if(amount == 0) return 1;
+        vector<int> dp(amount  + 1);
+        dp[0] = 1;
 
-        vector<vector<int>> dp(coins.size() + 1, vector<int>(amount + 1, 0));
-        dp[0][0] = 1;
+        for(auto coin : coins){
 
-        int answer = 0;
-
-        for(int i = 1; i <= coins.size(); i++){
-            int coin = coins[i - 1];
-            dp[i][0] = 1;
-            
-            for(int j = 1; j <= amount; j++){
-                if(j < coin) {
-                    dp[i][j] = dp[i - 1][j];
-                }else {
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - coin];
-                }
+            for(int number = coin; number <= amount; number++){
+                dp[number] += dp[number - coin];
             }
         }
 
-        // for(int i = 0; i < dp.size(); i++){
-        //     for(int j = 0; j < dp[i].size(); j++){
-        //         cout << dp[i][j] << " ";
-        //     }
-
-        //     cout << endl;
-        // }
-
-        return dp[coins.size()][amount];
+        return dp[amount];
     }
 };
