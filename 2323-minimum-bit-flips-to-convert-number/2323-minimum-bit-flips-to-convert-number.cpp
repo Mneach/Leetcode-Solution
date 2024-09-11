a9c0a7ae91b81c42a9742aa337209cfa081ed15f
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-09-11 17:48:03 +0700

    Time: 3 ms (41.47%), Space: 7.6 MB (44.87%) - LeetHub

--- 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
+++ 2323-minimum-bit-flips-to-convert-number/2323-minimum-bit-flips-to-convert-number.cpp
@@ -2,36 +2,14 @@ class Solution {
 public:
     int minBitFlips(int start, int goal) {
         
-        string bitStart = "";
-        string bitGoal = "";
-
-        while(start > 0){
-            bitStart += (start % 2) + '0';
-            start /= 2;
-        }
-
-        while(goal > 0){
-            bitGoal += (goal % 2) + '0';
-            goal /= 2;
-        }
-
-        while(bitGoal.length() < bitStart.length()){
-            bitGoal += '0';
-        }
-
-        while(bitStart.length() < bitGoal.length()){
-            bitStart += '0';
-        }
-        
-        reverse(bitStart.begin(), bitStart.end());
-        reverse(bitGoal.begin(), bitGoal.end());
-
         int answer = 0;
-
-        for(int i = 0; i < bitStart.length(); i++){
-            if(bitStart[i] != bitGoal[i]){
+        while(start > 0 || goal > 0){
+            if(start % 2 != goal % 2){
                 answer++;
             }
+
+            start /= 2;
+            goal /= 2;
         }
 
         return answer;
