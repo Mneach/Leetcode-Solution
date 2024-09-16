229a32f50cad78a01aeff47148e946b817400891
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-09-16 07:07:43 +0700

    Time: 67 ms (59.63%), Space: 18.8 MB (51.83%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
-        int prefixXOR = 0;
-        int characterMap[26] = {0};
-        characterMap['a' - 'a'] = 1;
-        characterMap['e' - 'a'] = 2;
-        characterMap['i' - 'a'] = 4;
-        characterMap['o' - 'a'] = 8;
-        characterMap['u' - 'a'] = 16;
-        vector<int> mp(32, -1);
-        int longestSubstring = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            prefixXOR ^= characterMap[s[i] - 'a'];
-            if (mp[prefixXOR] == -1 and prefixXOR != 0) mp[prefixXOR] = i;
-            longestSubstring = max(longestSubstring, i - mp[prefixXOR]);
+        unordered_map<int, int> ump;
+        int mask = 0;
+        int answer = 0;
+        
+        ump[0] = -1;
+        
+        for (int index = 0; index < s.size(); ++index) {
+            char c = s[index];
+            
+            if (c == 'a') mask ^= (1 << 0); 
+            if (c == 'e') mask ^= (1 << 1); 
+            if (c == 'i') mask ^= (1 << 2); 
+            if (c == 'o') mask ^= (1 << 3); 
+            if (c == 'u') mask ^= (1 << 4); 
+            
+            if (ump.find(mask) != ump.end()) {
+                answer = max(answer, index - ump[mask]);
+            } else {
+                ump[mask] = index;
+            }
         }
-
-        return longestSubstring;
+        
+        return answer;
     }
 };
