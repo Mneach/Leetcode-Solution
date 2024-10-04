78483a991dcf361602a02e1aed46891a4ca4b5f1
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-10-04 18:07:55 +0700

    Time: 68 ms (36.8%), Space: 57.5 MB (9.4%) - LeetHub

--- 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
     long long dividePlayers(vector<int>& skill) {
-        sort(skill.begin(), skill.end());
+        
+        if(skill.size() == 2){
+            return 1LL * skill[0] * skill[1];
+        }
 
+        unordered_map<long long,long long> ump;
+        long long total = 0;
+
+        for(auto data : skill){
+            ump[data]++;
+            total += data;
+        }
+
+        cout << total << endl;
+
+        total /= (skill.size() / 2);
         long long answer = 0;
+        
+        for(int i = 0; i < skill.size(); i++){
+            int number = skill[i];
+            if(ump[number] == 0) continue;
 
-        int left = 0;
-        int right = skill.size() - 1;
-        long long target = skill[left] + skill[right];
-
-        while(left < right){
-            long long temp = skill[left] + skill[right];
-            cout << target << " " << temp << endl;
-            if(temp != target) return -1;
-            
-            answer += (1LL * skill[left] * skill[right]);
-            left++;
-            right--;
+            if(ump[total - number] > 0){
+                ump[number]--;
+                ump[total - number]--;
+                answer += (1LL * number * (total - number));
+            }else{
+                return -1;
+            }
         }
 
         return answer;
