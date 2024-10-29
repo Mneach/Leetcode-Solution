06c9df1b8bbb620ddd6a5562120f7851af96b798
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-10-29 08:44:34 +0700

    Time: 31 ms (39.13%), Space: 74.6 MB (18.45%) - LeetHub

---
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    int maxMoves(vector<vector<int>>& grid, int i, int j, int m, int n,int prev, vector<vector<int>> & dp){
+       
+        if(i < 0 || j < 0 || i >= m || j >=  n) return 0;
+        
+        int currentCell = grid[i][j];
+        
+        if(prev >= currentCell) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int moveTopRight = maxMoves(grid, i - 1, j + 1, m, n,  currentCell, dp);
+        int moveRight = maxMoves(grid, i, j + 1, m, n, currentCell, dp);
+        int moveBottomRight = maxMoves(grid, i  +  1, j + 1, m, n, currentCell, dp);
+        
+        return dp[i][j] = max({moveTopRight, moveRight, moveBottomRight}) + 1;
+    }
+    
+    int maxMoves(vector<vector<int>>& grid) {
+        
+        int m = grid.size();
+        int n = grid[0].size();
+        int answer = 0;
+        
+        vector<vector<int>> dp(m, vector<int>(n , -1));
+        
+        for(int i = 0; i < grid.size(); i++){
+            int temp = maxMoves(grid, i , 0, m, n, 0, dp);
+            answer = max(temp , answer);
+        }
+        
+        return answer - 1;
+    }
+};
