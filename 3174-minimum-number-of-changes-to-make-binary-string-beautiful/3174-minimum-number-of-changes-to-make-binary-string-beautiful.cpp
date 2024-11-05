3f61e2b5b491c072c1a52ffb4c278b8bca208c84
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-11-06 05:36:08 +0700

    Time: 1 ms (54.05%), Space: 12.1 MB (67.88%) - LeetHub

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -2,25 +2,9 @@ class Solution {
 public:
     int minChanges(string s) {
         
-        int current = 0;
-        char take = s[0];
         int answer = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(take == '2') take = s[i];
-
-            if(take == s[i]){
-                current++;
-            }else{
-                // cout << i << " " << current << endl;
-                if(current % 2 == 1){
-                    answer++;
-                    current = 0;
-                }else{
-                    current = 1;
-                    take = s[i];
-                }
-            }
+        for(int i = 0; i < s.length(); i += 2){
+            if(s[i] != s[i + 1]) answer++; 
         }
 
         return answer;
