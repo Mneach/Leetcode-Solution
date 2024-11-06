eef239db7c1431722d3d1ec2acf2154482352d0d
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-11-06 21:08:34 +0700

    Time: 336 ms (5.04%), Space: 100.2 MB (5.05%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+
+    int countBits(int number){
+        int result = 0;
+
+        while(number > 0){
+            if(number % 2 == 1){
+                result++;
+            }
+            number /= 2;
+        }
+
+        return result;
+    }
+
+    bool canSortArray(vector<int>& nums) {
+        
+        unordered_map<int,int> ump;
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for(int i = 1; i <= 1000; i++){
+            int result = countBits(i);
+
+            ump[i] = result;
+        }
+
+        for(int i = 0; i < nums.size() - 1; i++){
+            int current = nums[i];
+            int next = nums[i + 1];
+
+            if(current > next){
+                if(ump[current] == ump[next]){
+                    swap(nums[i], nums[i + 1]);
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        vector<int> temp;
+
+        for(int i = 0; i < nums.size() - 1; i++){
+            int current = nums[i];
+            int next = nums[i + 1];
+
+            pq.push(current);
+
+            if(ump[current] != ump[next]){
+                while(pq.size() > 0){
+                    temp.push_back(pq.top());
+                    pq.pop();
+                }
+            }
+        }
+
+        pq.push(nums[nums.size() - 1]);
+
+        while(pq.size() > 0){
+            temp.push_back(pq.top());
+            pq.pop();
+        }
+
+        for(int i = 0; i < temp.size(); i++){
+            cout << temp[i] << " " << ump[temp[i]] << endl;
+        }
+
+        // check if sorted 
+        for(int i = 0; i < temp.size() - 1; i++){
+            if(temp[i] > temp[i + 1]) return false;
+        }
+
+        return true;
+    }
+};
