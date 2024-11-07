8784c8a256c7918203d88508486c3d10e6475667
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2024-11-07 10:43:31 +0700

    Time: 42 ms (21.64%), Space: 60 MB (99.4%) - LeetHub

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/2356-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -21,6 +21,12 @@ public:
 
 /*
 
+Intuition : 
+just need to check each bit from 2^0 until 2^32 / maximum value in the array
+that idea alwasy valid because : 
+1. & operation will keep the value to 1 if and only if bit-x & bit-y is equal to 1
+2. if the result of & operation is zero, that mean is not valid anymore
+
 Time Complexity : O(32 * N) + O(N) = O(N)
 O(32 * N) where 32 is the maximum bit of int and N total number in nums array
 O(N) -> because we need to loop from first index until last index in the array to get maximum value of that array
