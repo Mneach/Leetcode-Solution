352279e47434dcb163d1592f41490cc3a32a25d0
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-01-10 19:53:05 +0700

    Time: 331 ms (10.96%), Space: 166.4 MB (10.5%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        vector<string> answer;
+        unordered_map<char,int> ump;
+        
+        for (auto word : words2) {
+            unordered_map<char,int> temp;
+            for (int i = 0; i < word.size(); i++) {
+                temp[word[i]]++;
+            }
+
+            for (auto data : temp) {
+                char key = data.first;
+                int value = data.second;
+
+                if (ump[key] < value) ump[key] = value;
+            }
+        }
+
+        for (auto word : words1) {
+
+            unordered_map<char,int> temp;
+
+            for (int i = 0; i < word.size(); i++) {
+                temp[word[i]]++;
+            }
+
+            // compare
+            bool flag = true;
+            for (auto data : ump) {
+                char key = data.first;
+                int value = data.second;
+
+                if (temp[key] < value) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            if (flag) answer.push_back(word);
+        }
+
+        return answer;
+    }
+};
