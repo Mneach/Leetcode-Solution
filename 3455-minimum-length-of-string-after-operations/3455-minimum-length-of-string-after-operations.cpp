5136e50d7296acc1b58bf2d75df8bed73eebfcfc
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-01-13 07:28:56 +0700

    Time: 187 ms (5.24%), Space: 107.1 MB (5.24%) - LeetHub

---
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minimumLength(string s) {
+        unordered_map<char,vector<int>> ump;
+
+        for(int i = 0; i < s.length(); i++){
+            ump[s[i]].push_back(i);
+        }
+
+        int answer = 0;
+
+        for(auto data : ump){
+            if(data.second.size() % 2 == 0){
+                answer += 2;
+            }else if(data.second.size() % 2 == 1){
+                answer += 1;
+            }
+        }
+
+        return answer;
+    }
+};
