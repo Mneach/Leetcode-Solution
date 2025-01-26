5f06a211279c4c7529c18251a782a7c20810ee96
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-01-26 17:43:57 +0700

    Time: 335 ms (34.43%), Space: 191.5 MB (26.23%) - LeetHub

--- 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.cpp
+++ 2246-maximum-employees-to-be-invited-to-a-meeting/2246-maximum-employees-to-be-invited-to-a-meeting.cpp
@@ -1,20 +1,23 @@
 /*
-Intuition : 
-1. if the cyclic in 1 group is more than 2 nodes, that result should consider as an answer
+Intuition :
+1. if the cyclic in 1 group is more than 2 nodes, that result should consider as
+an answer
 
 Example
 favorite = [1,2,3,4,5,3]
 answer = 4
 cyclic result = 3 -> 4 -> 5 -> 3
 
-2. if the cyclic in 1 group is between 2 nodes, the result should be total node in that group
-favorite = [1,2,3,4,5,4]
-answer = 6
-cyclic result = 0 -> 1 -> 2 -> 3 -> 4 <-> 5
+2. if the cyclic in 1 group is between 2 nodes, the result should be total node
+in that group favorite = [1,2,3,4,5,4] answer = 6 cyclic result = 0 -> 1 -> 2 ->
+3 -> 4 <-> 5
 
-3. we alwasy can combine the group that have a cyclic in between 2 node, so this should consider as the result, since we can choose multiple group with this condition.
+3. we alwasy can combine the group that have a cyclic in between 2 node, so this
+should consider as the result, since we can choose multiple group with this
+condition.
 
-4. if there are more than 1 group in favorit answer, we just need to compare those groups using the rules above
+4. if there are more than 1 group in favorit answer, we just need to compare
+those groups using the rules above
 
 Time Complexity : O(N)
 -> where N is length of favorite array
@@ -22,18 +25,18 @@ Time Complexity : O(N)
 
 class Solution {
 public:
-
-    int bfs(int startNode, int parent, vector<vector<int>> &invertedGraph) {
+    int bfs(int startNode, int parent, vector<vector<int>>& invertedGraph) {
         int result = 0;
         // {node, length}
-        queue<pair<int,int>> q;
+        queue<pair<int, int>> q;
         q.push({startNode, 0});
 
-        while(!q.empty()) {
+        while (!q.empty()) {
             auto [node, length] = q.front();
             q.pop();
 
-            if (node == parent) continue;
+            if (node == parent)
+                continue;
 
             result = max(result, length);
             for (auto neighbor : invertedGraph[node]) {
@@ -46,35 +49,36 @@ public:
 
     int maximumInvitations(vector<int>& favorite) {
         vector<bool> visited(favorite.size());
-        vector<pair<int,int>> cycle2;
+        vector<pair<int, int>> cycle2;
 
         int firstAnswer = -1;
         for (int i = 0; i < favorite.size(); i++) {
-            if (visited[i] == true) continue;
+            if (visited[i] == true)
+                continue;
 
             int first = i;
             int current = i;
             set<int> bucket;
 
-            while(visited[current] == false) {
+            while (visited[current] == false) {
                 visited[current] = true;
                 bucket.insert(current);
                 current = favorite[current];
             }
 
-                int length = bucket.size();
-                while (first != current) {
-                    length--;
-                    first = favorite[first];
-                }
-                cout << length << endl;
+            int length = bucket.size();
+            while (first != current) {
+                length--;
+                first = favorite[first];
+            }
 
-                firstAnswer = max(length, firstAnswer);
+            firstAnswer = max(length, firstAnswer);
 
-                if (length == 2) {
-                    // cycle between node is 2, that means we can add more node in here
-                    cycle2.push_back({current, favorite[current]});
-                }
+            if (length == 2) {
+                // cycle between node is 2, that means we can add more node in
+                // here
+                cycle2.push_back({current, favorite[current]});
+            }
         }
 
         vector<vector<int>> invertedGraph(favorite.size());
@@ -90,12 +94,10 @@ public:
         for (auto data : cycle2) {
             int firstNode = data.first;
             int secondNode = data.second;
-            secondAnswer += bfs(firstNode, secondNode, invertedGraph) + bfs(secondNode, firstNode, invertedGraph) + 2;
+            secondAnswer += bfs(firstNode, secondNode, invertedGraph) +
+                            bfs(secondNode, firstNode, invertedGraph) + 2;
         }
-
-        cout << firstAnswer << " " << secondAnswer << endl;
 
         return max(firstAnswer, secondAnswer);
     }
 };
-
