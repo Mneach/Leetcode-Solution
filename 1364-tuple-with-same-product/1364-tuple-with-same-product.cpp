35e6fb2055eea7a22d4caab01bd1945b64116368
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-02-06 19:41:30 +0700

    Time: 329 ms (42.32%), Space: 105.2 MB (6.27%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.cpp
@@ -1,3 +1,31 @@
+
+/*
+
+Intuition : 
+
+We can use some math to solve this problem, we just need to find the pattern
+
+a * b == c * d
+(a * b) == (c * d)
+pair1 == pair2
+
+we can use count frequency, just put the multiply result inside the hashmap, use prefix sum to compute how many pair that we can construct
+
+total pair = total pair that we can construct
+formula = ((x - 1) * 4) * x
+
+(x - 1) -> is because we can only construct (x - 1) total pair
+* 4 -> is because we can swap between the position
+x -> is because we will have x pair
+
+0 = 0
+1 = 0
+2 = 8
+3 = 24
+4 = 48
+
+*/
+
 class Solution {
 public:
     int tupleSameProduct(vector<int>& nums) {
