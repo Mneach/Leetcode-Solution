4f1c7b67f5d4f7ad2850c66ff33d9ae42f57a494
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-03-09 16:47:33 +0700

    Time: 0 ms (100%), Space: 48.9 MB (93.33%) - LeetHub

---
+++ 3790-fruits-into-baskets-ii/3790-fruits-into-baskets-ii.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+        int answer = 0;
+        for (auto basket : baskets) {
+            int index = -1;
+            int result = INT_MIN;
+            for (int i = 0; i < fruits.size(); i++) {
+                int data = fruits[i];
+
+                if (data != INT_MIN && data <= basket) {
+                    index = i;
+                    result = data;
+                    break;
+                }
+            }
+
+            if (result == INT_MIN) answer++;
+            else fruits[index] = INT_MIN;
+        }
+
+        return answer;
+    }
+};
