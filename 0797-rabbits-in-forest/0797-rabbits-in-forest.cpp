dc5cc919816617a6a85dc3231ef4a5d2346ef01f
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-04-20 20:33:17 +0700

    Time: 0 ms (100%), Space: 12.3 MB (42.46%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numRabbits(vector<int>& answers) {
+        int answer = 0;
+        unordered_map<int,int> bucket;
+
+        for (auto answer : answers) {
+            bucket[answer]++;
+        }
+
+        for (auto data : bucket) {
+            int number = data.first;
+            int total = data.second;
+
+            // cout << number << " " << total << endl;
+
+            if (total % (number + 1) != 0) {
+                answer += ((total / (number + 1)) + 1) * (number + 1);
+            } else {
+                answer += (total / (number + 1)) * (number + 1);
+            }
+
+            // cout << "RESULT : " << " " <<  number << " " << answer << endl;
+        }
+
+        return answer;
+    }
+};
