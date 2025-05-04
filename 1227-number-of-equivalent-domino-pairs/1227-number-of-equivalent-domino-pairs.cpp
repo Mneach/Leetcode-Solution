53fc449bc5d2f5f2786eb2754824d2a0b3464dc6
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-05-04 07:40:29 +0700

    Time: 8 ms (35%), Space: 28.9 MB (8.75%) - LeetHub

---
+++ 1227-number-of-equivalent-domino-pairs/1227-number-of-equivalent-domino-pairs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        int answer = 0;
+        vector<int> preCompute(dominoes.size() + 1, 0);
+        vector<vector<int>> bucket(10, vector<int>(10, 0));
+
+        for (int i = 2; i < preCompute.size(); i++) {
+            preCompute[i] = preCompute[i - 1] + i - 1; 
+        }
+
+        for (auto domino : dominoes) {
+            int first = domino[0];
+            int second = domino[1];
+
+            bucket[min(first, second)][max(first, second)]++;
+        }
+
+        for (int i = 1; i < bucket.size(); i++) {
+            for (int j = 1; j < bucket[i].size(); j++) {
+                if (bucket[i][j] == 0) continue;
+
+                // cout << i << " " << j << " " << bucket[i][j] << endl;
+                answer += preCompute[bucket[i][j]];
+            }
+        }
+
+        return answer;
+    }
+};
