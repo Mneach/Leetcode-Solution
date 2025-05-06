ac4c3b80cd9ed29045b62c0f4793082aee232156
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-05-06 07:18:53 +0700

    Time: 0 ms (100%), Space: 20.5 MB (58.11%) - LeetHub

---
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> answer(nums.size());
+        
+        
+        for(int i = 0; i < nums.size(); i++){
+            answer[i] = nums[nums[i]];
+        }
+        
+        return answer;
+    }
+};
