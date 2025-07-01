1724c12a9520f33cb231e10af321cfa4b3abf41b
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-07-01 08:59:07 +0700

    Time: 99 ms (62.52%), Space: 101.9 MB (47.28%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.cpp
+++ 0948-sort-an-array/0948-sort-an-array.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        
-        priority_queue <int, vector<int>, greater<int>> pq;
-        
-        for(int i = 0; i < nums.size(); i++){
-            pq.push(nums[i]);
+        unordered_map<int,int> bucket;
+        int minimum = INT_MAX;
+        int maximum = INT_MIN;
+
+        for (auto num : nums) {
+            minimum = min(num, minimum);
+            maximum = max(num, maximum);
+            bucket[num]++;
         }
-        
+
+        int index = 0;
         vector<int> answer;
-        
-        while(!pq.empty()){
-            answer.push_back(pq.top());
-            pq.pop();
+        for (int i = minimum; i <= maximum; i++) {
+            while (bucket[i] > 0) {
+                answer.push_back(i);
+                bucket[i] -= 1;
+            }
         }
         
         return answer;
