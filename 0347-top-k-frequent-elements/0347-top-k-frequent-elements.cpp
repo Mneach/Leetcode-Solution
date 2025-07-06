b84cf62c583912750be5acb7c034f56483ca0b46
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-07-07 04:38:19 +0700

    Time: 7 ms (16.82%), Space: 20 MB (7.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,20 +1,41 @@
+/*
+
+How to solve : 
+1. count the number frequency using hash tabel
+2. put into the array based on the number frequency
+3. get the answer until current answer size == k
+
+Time complexity : O(N)
+Memory complexity : O(N)
+
+*/
+
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int,int> bucket;
+        unordered_map<int,int> counter;
+        vector<vector<int>> bucket(nums.size() + 1);
         vector<int> answer;
-        for (auto num : nums) bucket[num]++;
 
-        priority_queue<pair<int,int>> pq;
-
-        for (auto data : bucket) {
-            pq.push({data.second, data.first});
+        for (auto num : nums) counter[num]++;
+        
+        for (auto data : counter) {
+            int key = data.first;
+            int value = data.second;
+            
+            bucket[value].push_back(key);
         }
 
-        while (k > 0 && pq.size() > 0) {
-            answer.push_back({pq.top().second});
-            k--;
-            pq.pop();
+        for (int i = bucket.size() - 1; i >= 0 && k > 0; i--) {
+            if (bucket[i].size() != 0) {
+                int index = 0;
+                while (k > 0 && index < bucket[i].size()) {
+                    answer.push_back(bucket[i][index]);
+                    k--;
+                    index++;
+                }
+            }
         }
 
         return answer;
