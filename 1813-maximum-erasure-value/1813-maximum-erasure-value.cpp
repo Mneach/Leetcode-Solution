8e364cd05e1529553e43f715ed445fa594d40a8d
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-07-06 08:06:47 +0700

    Time: 1247 ms (5.29%), Space: 103.3 MB (86.05%) - LeetHub

---
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int maximumUniqueSubarray(vector<int>& nums) {
+        // key : number
+        // value : index
+        unordered_map<int,int> bucket;
+        int answer = 0;
+
+        int left = 0;
+        for (int right = 0; right < nums.size(); right++) {
+
+            if (bucket.count(nums[right]) > 0 && bucket[nums[right]] >= left) {
+                int temp = 0;
+                while (left < right) {
+                    temp += nums[left];
+                    left++;
+                }
+                answer = max(answer, temp);
+                left = bucket[nums[right]] + 1;
+            }
+            
+            bucket[nums[right]] = right;
+        }
+
+        int temp = 0;
+        while (left < nums.size()) {
+            temp += nums[left]; 
+            left++;
+        }
+
+        return max(answer, temp);
+    }
+};
