d34f06275f138e7bb9b7e23dd581e4bcda2f9b8c
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-07-06 08:38:36 +0700

    Time: 66 ms (79.22%), Space: 103.3 MB (86.05%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        // key : number
-        // value : index
         unordered_map<int,int> bucket;
-        int answer = 0;
 
+        int answer = 0;
         int left = 0;
         int temp = 0;
         for (int right = 0; right < nums.size(); right++) {
-
+            
             if (bucket.count(nums[right]) > 0 && bucket[nums[right]] >= left) {
-                while (left < right) {
-                    temp += nums[left];
+                answer = max(temp, answer);
+                int stopPoint = bucket[nums[right]];
+                while (left < stopPoint + 1) {
+                    temp -= nums[left];
                     left++;
                 }
-                answer = max(answer, temp);
-                left = bucket[nums[right]] + 1;
-                temp = 0;
             }
-            
-            bucket[nums[right]] = right;
-        }
 
-        temp = 0;
-        while (left < nums.size()) {
-            temp += nums[left]; 
-            left++;
+            bucket[nums[right]] = right;
+            temp += nums[right];
         }
 
         return max(answer, temp);
