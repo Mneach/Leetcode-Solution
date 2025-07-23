4637dee47816b01b7c6cab534be2d3b0ebfbdc6f
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-07-23 11:29:20 +0700

    Time: 0 ms (100%), Space: 9 MB (94.81%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> summaryRanges(vector<int>& nums) {
+        vector<string> result;
+        int n = nums.size();
+
+        for (int i = 0; i < n; ++i) {
+            int start = nums[i];
+
+            // Advance `i` while the next number is consecutive
+            while (i + 1 < n && nums[i + 1] == nums[i] + 1) {
+                ++i;
+            }
+
+            // If the range has more than one number
+            if (nums[i] != start) {
+                result.push_back(to_string(start) + "->" + to_string(nums[i]));
+            } else {
+                result.push_back(to_string(start));
+            }
+        }
+
+        return result;
+    }
+};
