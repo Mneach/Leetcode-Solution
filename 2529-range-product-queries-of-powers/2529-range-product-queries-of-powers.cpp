fef668ba3daf5e428c8d4ef408628f6d2b205aed
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-08-11 20:04:01 +0700

    Time: 84 ms (29.26%), Space: 165.5 MB (5.85%) - LeetHub

--- 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
+++ 2529-range-product-queries-of-powers/2529-range-product-queries-of-powers.cpp
@@ -7,7 +7,7 @@ public:
         int mod = 1e9 + 7;
         for (int i = 0; i < 32; i++) {
             if (((n >> i) & 1) == 1) {
-                cout << i << endl;
+                // cout << i << endl;
                 int temp = pow(2, i); 
                 if (numbers.size() == 0) {
                     numbers.push_back(temp);
