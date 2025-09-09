61877847a2e5e75d35870de7cf7dfb27cc7fb0ad
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-09 22:25:17 +0700

    Time: 15 ms (30.69%), Space: 9.5 MB (39.85%) - LeetHub

--- 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -3,8 +3,6 @@ public:
     int mod = 1e9 + 7;
 
     int recursive(int curr, int &n, int &delay, int &forget, vector<int> &dp) {
-        if (curr > n) return 0;
-
         if (dp[curr] != -1) return dp[curr];
 
         int start = curr + delay;
@@ -24,9 +22,6 @@ public:
 
     int peopleAwareOfSecret(int n, int delay, int forget) {
         vector<int> dp(n + 1, -1);
-        
-        int answer = recursive(1, n, delay, forget, dp);
-
-        return answer;
+        return recursive(1, n, delay, forget, dp);
     }
 };
