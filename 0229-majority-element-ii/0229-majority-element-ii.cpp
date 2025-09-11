b19dc70d917c7a52c2b9e0374cacffbb54887073
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-11 19:45:06 +0700

    Time: 8 ms (33.18%), Space: 26.4 MB (29.67%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,21 +1,41 @@
+/*
+
+How to solve the problem : 
+
+# Hash table approach
+1. loop for every number in nums array
+2. put nums[i] intp hashtable with key = nums[i] and value = total of number x in that nums of array
+3. put all of the numbers that appear more than n / 3 into array of answers
+
+Time complexity : O(N)
+where N -> size of nums array
+
+Memory complexity : O(N)
+where N -> size of nums array
+
+*/
+
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int minimum = nums.size() / 3;
-        
-        unordered_map<int,int> arr;
-        
-        for(int i = 0; i < nums.size(); i++){
-            arr[nums[i]]++;
-        }
-        
         vector<int> answer;
-        for(auto data : arr){
-            if(data.second > minimum){
-                answer.push_back(data.first);
+        unordered_map<int,int> bucket; 
+
+        for (int i = 0; i < nums.size(); i++) {
+            int number = nums[i];
+            bucket[number]++;
+        }
+
+        int totalMinimum = nums.size() / 3;
+        for (auto data : bucket) {
+            int key = data.first;
+            int value = data.second;
+
+            if (value > totalMinimum) {
+                answer.push_back(key);
             }
         }
-        
+
         return answer;
     }
 };
