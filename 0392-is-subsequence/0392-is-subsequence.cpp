e8fe1de398ea2a125c8a8929b59af753a439c83d
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-11 13:51:06 +0700

    Time: 0 ms (100%), Space: 8.5 MB (66.73%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.cpp
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -1,12 +1,38 @@
+/*
+
+How to solve the problem
+
+# Two Pointer Technique
+1. Loop for every character in s and t
+   - if s[indexS] == t[indexT], then increase indexS and indexT by 1
+   - otherwise indexT by 1
+2. to find the answer we need to check if indexS == s.length()
+
+Time Complexity : O(min(s.length(), t.length)) -> O(N)
+N = min(s.length(), t.length())
+
+Memory Comlexity : O(1)
+
+*/
+
+
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int currentIndex = 0;
-        
-        for(int i = 0; i < t.length(); i++){
-            if(s[currentIndex] == t[i]) currentIndex++;
+
+        int indexS = 0;        
+        int indexT = 0;
+
+        while (indexS < s.length() && indexT < t.length()) {
+            
+            if (s[indexS] == t[indexT]) {
+                indexS++;
+                indexT++;
+            } else {
+                indexT++;
+            }
         }
-        
-        return s.length() == currentIndex;
+
+        return s.length() == indexS;
     }
 };
