94f982f8bb9dfd917a713150cb4183a6de6ad609
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-11 07:23:42 +0700

    Time: 3 ms (40.58%), Space: 13.5 MB (33.25%) - LeetHub

--- 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.cpp
+++ 2572-append-characters-to-string-to-make-subsequence/2572-append-characters-to-string-to-make-subsequence.cpp
@@ -1,13 +1,37 @@
+/* 
+
+How to solve : 
+
+# Using two pointer technique
+1. we need to check if s[indexS] == t[indexT]
+   - if thats equal, we need to increase indexS and indexT
+   - otherwise, we just need to increase indexS
+2. to compute the answer, we can use this formula
+   - answer = t.length() - indexT
+
+time complexity : O(N)
+N -> min(s.length(), t.length())
+
+memory complexity : O(1)
+
+*/
+
+
 class Solution {
 public:
     int appendCharacters(string s, string t) {
-        
-        int currIndex = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] == t[currIndex]) currIndex++;
+        int indexS = 0;        
+        int indexT = 0;
+
+        while (indexS < s.length() && indexT < s.length()) {
+            if (s[indexS] == t[indexT]) {
+                indexS++;
+                indexT++;
+            } else {
+                indexS++;
+            }
         }
-        
-        return t.length() - currIndex;
+
+        return t.length() - indexT;
     }
 };
