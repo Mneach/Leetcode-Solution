827a1a08b38a807614e6d525022cc94d850554ca
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-12 13:21:34 +0700

    Time: 0 ms (100%), Space: 17.3 MB (42.56%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -1,18 +1,31 @@
+/*
+
+How to solve the problem : 
+1. start loop from index 11 until detail.length() - 2
+   - the reason is because of age start from index 11 and will end in index (detail.length() - 2)
+2. calculate the age using this formula
+   - age = (age * multiply * 10) + (detail[i] - '0')
+Time Complexity : O(N)
+N -> length between index 11 until detail.length() - 2
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int countSeniors(vector<string>& details) {
-        
         int answer = 0;
 
-        for(int i = 0; i < details.size(); i++){
-
-            string data = details[i];
-
-            int age = (data[11] - '0') * 10 + (data[12] - '0');
-
-            if(age > 60){
-                answer++;
+        for (auto detail : details) {
+            int age = 0;
+            int multiply = 0;
+            for (int i = 11; i < detail.length() - 2; i++) {
+                age = (age * multiply * 10) + (detail[i] - '0');
+                multiply++;
             }
+
+            if (age > 60) answer++;
         }
 
         return answer;
