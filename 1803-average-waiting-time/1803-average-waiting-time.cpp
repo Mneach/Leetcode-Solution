75acbdb0be88e04edc1b5fe35e4fe579de64969d
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-12 13:36:28 +0700

    Time: 31 ms (24.2%), Space: 87 MB (18.84%) - LeetHub

--- 1803-average-waiting-time/1803-average-waiting-time.cpp
+++ 1803-average-waiting-time/1803-average-waiting-time.cpp
@@ -1,26 +1,42 @@
+/*
+
+How to solve the problem : 
+1. loop from the first customer until the last customer, since the customer data is already sorted by time non descreasing and if the time is equal we just need to serve the customer in the order
+2. for every loop we need to do these processes
+   2.1 if the current time <= arrival time
+   2.2.1 set the current time to arrival time
+   
+   2.2 add the current time with the time that we need to prepare a customer food
+   2.3 to calculate how long the customer will wait, we can use this formula (current time - arrival time) and put the result into the sum variable
+
+3. to know the answer we will divide the sum variable by total number of customers
+
+Time Complexity : O(N)
+N -> array of customers
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
-
-    long long int maxLL(long long a, long long b){
-        if(a > b) return a;
-        else return b;
-    }
-
     double averageWaitingTime(vector<vector<int>>& customers) {
-        long long int idle1,idle2;
-        idle1 = idle2 = 0;
+        long long sum = 0;        
+        long long currentTime = 0; 
 
-        for(int i = 0; i < customers.size(); i++){
-            int arrivalTime = customers[i][0];
-            int time = customers[i][1];
+        for (auto customer : customers) {
+            int arrivalTime = customer[0];
+            int time = customer[1];
 
-            idle1 = maxLL(arrivalTime, idle1) + time;
+            if (currentTime <= arrivalTime) {
+                currentTime = arrivalTime;
+            }
 
-            idle2 += idle1 - arrivalTime;
+            currentTime += time;
+
+            sum += currentTime - arrivalTime;
         }
 
-        double answer = (double) idle2 / customers.size();
-
-        return answer;
+        return (1.0 * sum) / (1.0 * customers.size());
     }
 };
