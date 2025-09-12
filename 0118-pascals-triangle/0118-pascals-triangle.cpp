b6c4598ed4b3690be398c8fe4382af7cf6bce921
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-12 19:26:23 +0700

    Time: 0 ms (100%), Space: 9.6 MB (64.46%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,20 +1,42 @@
+/*
+
+How to solve the problem : 
+1. use row 1 as a basecase
+2. every time we add a new row, put 1 at left and right side as a default value
+3. sum the number from previous row and put that result in the new row 
+
+Time Complexity : O(N)
+N -> total number in that pascal triangle
+
+Memory Complexity : O(N)
+N -> total number in that pascal triangle
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
-        
-        vector<vector<int>> dp;
-        
-        for(int i = 0; i < numRows; i++){
-            
-            vector<int> row(i + 1 , 1);
-            
-            for(int j = 1; j < i; j++){
-                row[j] = dp[i - 1][j - 1] + dp[i - 1][j]; 
+        vector<vector<int>> array;
+
+        // basecase
+        array.push_back({1});
+        numRows--;
+
+        while (numRows > 0) {
+            vector<int> lastRow = array[array.size() - 1];
+            vector<int> newRow;
+            newRow.push_back({1});
+
+            for (int i = 1; i < lastRow.size(); i++) {
+                newRow.push_back(lastRow[i] + lastRow[i - 1]);
             }
-            
-            dp.push_back(row);
+
+            newRow.push_back({1});
+
+            array.push_back(newRow);
+            numRows--;
         }
-        
-        return dp;
+
+        return array;
     }
 };
