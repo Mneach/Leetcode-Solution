144fb7d920d2fbe97dc367edcd2810f2fff34174
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-15 20:03:31 +0700

    Time: 0 ms (100%), Space: 24 MB (74.33%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.cpp
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -1,37 +1,67 @@
+/*
+
+How to solve the problem : 
+
+# Using basic logic
+1. loop from left index of flower bed until right index of flower bed
+2. for every index we need to do these steps
+   1. check if the left index of the current index (index - 1) == 0, then we can mark left as valid
+   2. check if the right index of the current index (index + 1) == 0, then we can mark right as valid
+   3. if left and right are valid, we can put the flower in that index and change the current value from 0 to 1, otherwise just return false
+
+edge cases : 
+1. what if index == 0 ? 
+   - if that the case, we can mark left as valid 
+2. what if index == flowerbed.size() ? 
+   - if that the case we also can mark right as valid
+
+Time Complexity : O(N)
+N -> size of flowerbed array
+
+Memory Comlexity : O(1)
+
+*/
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
         
-        int length = flowerbed.size();
-   
-        if(length == 1) {
-            if(n == 1) return flowerbed[0] == 0;
-            else return true;
-        }
-        
-        for(int i = 0; i < length; i++){
-            
-            if(flowerbed[i] == 1) continue;
-            
-            if(i == 0){
-                if(flowerbed[i + 1] == 0) {
-                    flowerbed[i] = 1;
-                    n--;
-                }
-            }else if(i == length - 1){
-                if(flowerbed[i - 1] == 0) {
-                    flowerbed[i] = 1;
-                    n--;
-                }
-            }else{
-                if(flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0) {
-                    flowerbed[i] = 1;
-                    n--;
-                }
+        for (int i = 0; i < flowerbed.size(); i++) {
+            bool left = false;
+            bool right = false;
 
+            if (flowerbed[i] == 1) {
+                continue;
             }
+
+            if (i == 0) {
+                left = true;
+            } 
+            
+            if (i == flowerbed.size() - 1) {
+                right = true;
+            }
+
+            if (i > 0) {
+                // check left value
+                if (flowerbed[i - 1] == 0) {
+                    left = true;
+                }
+            } 
+            
+            if (i < flowerbed.size() - 1) {
+                if (flowerbed[i + 1] == 0) {
+                    right = true;
+                }
+            }
+
+            if (left && right) {
+                n--;
+                flowerbed[i] = 1;
+            }
+
+            if (n == 0) break;
         }
-        
+
         return n <= 0;
     }
 };
