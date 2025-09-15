de1d408bb9b640f9c157191225b2c0a0002387cb
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-15 20:40:56 +0700

    Time: 0 ms (100%), Space: 10 MB (26.34%) - LeetHub

--- 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.cpp
@@ -1,26 +1,43 @@
+/*
+
+How to solve the problem : 
+
+# Using hashtable
+1. Put all the charcter in string s into the hashtable with key = character, value = total character x appear in string s
+2. loop from hash table to get the highest and lowest value for odd and even freq data
+3. get the maximum value from those data
+
+Time Complexity : O(N + (M + M))
+N -> length of string S
+M -> total different character that appear in string s
+
+Memory Complexity : O(M)
+M -> total differenet character that appear in string s
+
+*/
+
 class Solution {
 public:
     int maxDifference(string s) {
+        unordered_map<char,int> bucket;
 
-        vector<int> bucket(26, 0);
-        
-        for (int i = 0; i < s.length(); i++) {
-
-            int index = s[i] - 'a';
-            bucket[index]++;
+        for (auto c : s) {
+            bucket[c]++;
         }
-
-        int odd = INT_MIN;
+        
+        int odd = 0;
         int even = INT_MAX;
+        
+        for (auto data : bucket) {
+            int freq = data.second;
 
-        for (int i = 0; i < bucket.size(); i++) {
-            if (bucket[i] % 2 == 0 && bucket[i] > 0) {
-                even = min(even, bucket[i]);
+            if (freq % 2 == 1) {
+                odd = max(odd, freq);
             } else {
-                odd = max(odd, bucket[i]);
+                even = min(even, freq);
             }
         }
-
+        
         return odd - even;
     }
 };
