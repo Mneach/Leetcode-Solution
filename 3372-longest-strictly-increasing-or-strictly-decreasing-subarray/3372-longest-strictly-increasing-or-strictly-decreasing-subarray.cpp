a4cc961540acc144764b375592ecf01a5b531e91
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-16 15:06:43 +0700

    Time: 0 ms (100%), Space: 28.1 MB (9.27%) - LeetHub

--- 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ 3372-longest-strictly-increasing-or-strictly-decreasing-subarray/3372-longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -1,29 +1,39 @@
+/*
+
+How to solve the problem : 
+1. looping from first index of nums array to last index of nums array to find longest increasing and decreasing sub array
+
+Time Complexity : O(N)
+N -> size of an array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
-
         int answer = 1;
-        // search for increasing
-        
-        int left = 0;
-        for(int right = 1; right < nums.size(); right++){
-            if(nums[right - 1] < nums[right]){
-                answer = max(right - left + 1, answer);
-            }else{
-                left = right;
+        int inc = 1;
+        int dec = 1;
+
+        for (int i = 1; i < nums.size(); i++) {
+
+            if (nums[i] > nums[i - 1]) {
+                inc++;
+                answer = max(answer, dec);
+                dec = 1;
+            } else if (nums[i] < nums[i - 1]) {
+                dec++;
+                answer = max(answer, inc);
+                inc = 1;
+            } else {
+                answer = max(answer, max(dec, inc));
+                dec = 1;
+                inc = 1;
             }
         }
-        
-        // serach for descreasing
-        left = 0;
-        for(int right = 1; right < nums.size(); right++){
-            if(nums[right - 1] > nums[right]){
-                answer = max(right - left + 1, answer);
-            }else{
-                left = right;
-            }
-        }
-        
-        return answer;
+
+        return max(answer, max(inc, dec));
     }
 };
