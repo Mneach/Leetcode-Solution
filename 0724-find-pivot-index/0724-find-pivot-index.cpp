a76f26788542bb4c901477584c45bc3e9752666d
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-17 19:37:45 +0700

    Time: 0 ms (100%), Space: 35.6 MB (95.44%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -3,9 +3,10 @@
 How to solve the problem : 
 1. loop from i = 0 until i < nums.size() to construct prefixSumLeft
 2. loop from i = nums.size() - 1 until i = 1, for every process we need to do this to get the answer
+   - decrease prefixSumLeft by nums[i]
    - if prefixSumleft == prefixSumRight then we mark that index as an aswer
-   - decrease prefixSumLeft by nums[i];
-   - increase prefixSumRight by nums[i];
+   - increase prefixSumRight by nums[i]
+3. check if the answer == nums.size(), that means we cannot find the answer so just return -1, otherwise return the answer value
 
 Time Complexity : O(N + N)
 N -> size of nums array
