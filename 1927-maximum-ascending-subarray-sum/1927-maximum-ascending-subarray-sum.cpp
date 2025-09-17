8cbcedf647ca74324725f5abfe7023c2122ee358
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-17 14:18:38 +0700

    Time: 0 ms (100%), Space: 11 MB (96.62%) - LeetHub

--- 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -1,19 +1,40 @@
+/*
+
+How to solve the problem : 
+
+# Native Approach 
+1. loop from first index of nums array until last index of nums array
+   - if nums[i] <= nums[i + 1], calculate the sum of increasing sub array, if the sum > current answer, then update the current ansewr
+   - else if (nums[i] > nums[i + 1]), update the sum value
+
+edge cases : 
+1. what if all the number in nums array are increasing ? 
+   - outside the loop we need to update the answer value
+
+Time Complexity : O(N)
+N -> size of nums array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int maxAscendingSum(vector<int>& nums) {
-        int answer = 0;
+        int answer = nums[0];
+        int sum = nums[0];
 
-
-        int temp = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (i > 0 && nums[i] <= nums[i - 1]) {
-                answer = max(answer, temp);
-                temp = nums[i];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                answer = max(answer, sum);
+                sum = nums[i];
             } else {
-                temp += nums[i];
+                sum += nums[i];
             }
         }
 
-        return max(answer, temp);
+        answer = max(answer, sum);
+
+        return answer;
     }
 };
