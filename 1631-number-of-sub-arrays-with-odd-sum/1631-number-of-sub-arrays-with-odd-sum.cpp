35dca74cfa32f71bb405ef2756c4a8383d5e25c0
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-17 20:36:14 +0700

    Time: 4 ms (54.62%), Space: 112.1 MB (49.32%) - LeetHub

--- 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -1,22 +1,53 @@
+/*
+
+How to solve the problem : 
+idea : 
+- odd + even = odd
+- odd + odd = even
+- even + even = even
+
+To create the odd sum, we need to have atleast (1, 3, 5, ...), where n value start from 1
+
+1. Define variable, currentResult, prevOddResult, twoPrevOddResult
+2. Loop from the first index until the last index, need to do these steps
+   - count how many even number that we have until we meet odd number
+   - if we meet odd number, we can compute by using this formula
+      formula : ((totalEven + 1) + twoPrevOddResult) % 10^9 + 7
+   - update twoPrevoddResult = prevOddResult
+   - prevOddResult = currentResult
+   - currentResult = totalEven + 1 + twoPrevOddResult
+   - dont forget to use module by 10^9 + 7
+
+Time Complexity : O(N)
+N -> size of arr
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int numOfSubarrays(vector<int>& arr) {
-        constexpr int MOD = 1e9 + 7;
-        int count = 0, prefixSum = 0;
-        int oddCount = 0, evenCount = 1;  // evenCount starts as 1 since initial sum (0) is even
+        int mod = 1e9 + 7;
+        int answer = 0;
+        int currentResult = 0;
+        int prevOddResult = 0;
+        int prevTwoOddResult = 0;
 
-        for (int num : arr) {
-            prefixSum = (prefixSum + num) % 2;
-            
-            if (prefixSum == 0) {
-                count = (count + oddCount) % MOD;
-                ++evenCount;
+        int totalEven = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr[i] % 2 == 0) {
+                totalEven++;
             } else {
-                count = (count + evenCount) % MOD;
-                ++oddCount;
+                currentResult = ((totalEven + 1) + prevTwoOddResult) % mod;
+                prevTwoOddResult = prevOddResult;
+                prevOddResult = currentResult;
+                totalEven = 0;
             }
-        }
-        
-        return count;
+
+            answer = (answer + currentResult) % mod;
+        }        
+
+        return answer;
     }
 };
