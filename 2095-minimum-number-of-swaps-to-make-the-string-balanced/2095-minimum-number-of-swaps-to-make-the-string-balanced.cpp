64c5a201d6441e000a3168faea17d38c15cdfe14
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-18 06:50:15 +0700

    Time: 14 ms (50.83%), Space: 34.2 MB (41.98%) - LeetHub

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/2095-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,49 +1,38 @@
 /*
 
 How to solve the problem : 
-1. loop from first index of s until last index of s
-2. if we meet the unmatch bracket we need to store the current count to the count variable
-   - umatch bracket = "]["
-3. if we meet match bracket we need to decrease the current count by 1, since we will ignore the match bracket
-   - match bracket = "[]"
-4. to calculate how many minumum swap that we need, we can use this formula
-   - formula : (n / 2) + (n % 2)
-   - n / 2 -> because of for every swap, the best case scenario is to get 2 match bracket
-   - n % 2 -> because of for every odd count value, we need to add + 1 swap operation
+
+1. Loop from first indexo f S until last index of S
+2. for every index we need to check wheather s[i] value is '[' or ']'
+   - if s[i] is '[', count decrease by 1
+   - if s[i] is ']', count increase by 1 
+
+Note : 
+1. The definition of count is how many unmatch bracket in string s
 
 Time Complexity : O(N)
-N -> size of string S
+N -> size of string s
 
 Memory Complexity : O(1)
+
 
 */
 
 class Solution {
 public:
     int minSwaps(string s) {
-        int count = 0;        
-        int currentCount = 0;
+        int count = 0;
+        int unmatchBracket = 0;
 
         for (int i = 0; i < s.length(); i++) {
-
-            if (i > 0 && s[i - 1] == ']' && s[i] == '[' && currentCount < 0) {
-                count += abs(currentCount);
-                currentCount = 0;
-            }
-            
             if (s[i] == '[') {
-                currentCount++;
+                count--;
             } else {
-                currentCount--;
+                count++;
             }
 
+            unmatchBracket = max(unmatchBracket, count);
         }
-
-        if (currentCount != 0) {
-            count += currentCount;
-        }
-
-        int unmatchBracket = count / 2;
 
         return (unmatchBracket / 2) + (unmatchBracket % 2);
     }
