478daa3da152d8de123e2ce90463be04e2252590
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-17 10:59:37 +0700

    Time: 469 ms (12.61%), Space: 18.1 MB (14.71%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.cpp
@@ -1,36 +1,61 @@
+/*
+
+How to solve the problem : 
+1. Loop from first index of string s until last index of string s
+2. Put the character into the hasthable with key = s[i] and value is pair<first index, last index>
+3. Loop for every data in hashtable, we need to do these steps
+   - Create unique palindrome template using (key _ key)
+   - Looping from first index + 1 until last index -1, those index we can get from hashtable[key]
+   - For every index put the current character s[i] into the middle of palindrome index
+   - Check if the current palindrome is already exists in cache hash table, we need to skip
+   - otherwise we need to put that palindrome into the hashtbale and increase the answer value by 1
+
+Time Complexity : O(N * M)
+N -> total english characters that appear in string S
+M -> length of String S
+
+Memory Complexity : O(N + M / 3)
+N -> total english characters that appear in string s
+M -> length os string s
+M / 3 -> because we need to create unique palindrome of lenght three
+
+*/
+
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<pair<int,int>> arr(26, make_pair(-1, -1));
+        unordered_map<char,pair<int,int>> bucket;
+        unordered_map<string,int> cache;
         int answer = 0;
 
-
         for (int i = 0; i < s.length(); i++) {
-
-            int index = s[i] - 'a';
-
-            if (arr[index].first == -1) {
-                arr[index].first = i;
+            if (bucket.count(s[i]) > 0) {
+                // update the last index
+                bucket[s[i]].second = i;
             } else {
-                arr[index].second = i;
+                // put the character along with the index into bucket hash table
+                bucket[s[i]] = {i, i};
             }
         }
 
-        for (int i = 0; i < 26; i++) {
+        for (auto data : bucket) {
+            char key = data.first;
+            auto index = data.second;
+            int firstIndex = index.first;
+            int lastIndex = index.second;
             
-            int start = arr[i].first;
-            int end = arr[i].second;
+            string palindrome = "xxx";
+            palindrome[0] = key;
+            palindrome[2] = key;
 
-            if (start == -1 || end == -1) continue;
+            for (int i = firstIndex + 1; i < lastIndex; i++) {
+                palindrome[1] = s[i];
 
-            // cout << i << " " << start << " " << end << endl;
-
-            set<char> st;
-            for (int index = start + 1; index < end; index++) {
-                st.insert(s[index]);
+                if (cache.count(palindrome) == 0) {
+                    answer++;
+                    cache[palindrome] = 1;
+                }
             }
-
-            answer += st.size();
         }
 
         return answer;
