3a7a33f6ef53d6e815e262a75840bc43fec9831d
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-18 13:02:01 +0700

    Time: 2 ms (81.83%), Space: 15 MB (99.28%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,31 +1,48 @@
+/*
+
+How to solve the problem : 
+
+# Using Sorting Algorithm
+1. To know which number must be come the first number, we can compare the number using this idea
+   - number 1 = 30
+   - number 2 = 3
+   - number 1 + number 2 > number 2 + number 1, then number 1 must be come first than number 2, otherwise number 2 must be come first than number 1
+   - ex : 303 > 330, then we need to swap number 2 with number 1 position
+2. Sorting the nums array using merge sort algorithm and use condition above 
+3. Loop from the first index of array until the last index of array to construct the answer
+   - need to transform nums[i] from number to string
+
+edge cases :
+1. what if nums only contain [0,0]
+   - we should return 0
+
+Time Complexity : O(N log n)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
+
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
         
-        vector<string> temp;
+        sort(nums.begin(), nums.end(), [](int first, int second) {
+            string firstNumber = to_string(first);
+            string secondNumber = to_string(second);
 
-        for(auto data : nums){
-            temp.push_back(to_string(data));
-        }
-
-        sort(temp.begin(), temp.end(),[](string &first, string &second){
-            return first + second > second + first;
+            return firstNumber + secondNumber > secondNumber + firstNumber;
         });
 
         string answer = "";
-        bool zero = true;
-        for(auto data : temp) {
-            if(stoi(data) > 0){
-                zero = false;
+        for (int i = 0; i < nums.size(); i++) {
+            if (answer == "" && nums[i] == 0) {
+                return "0";
             }
-            answer += data;
+            answer += to_string(nums[i]);
         }
 
-        
-        if(zero){
-            return "0";
-        }else{
-            return answer;
-        }
+        return answer;
     }
 };
