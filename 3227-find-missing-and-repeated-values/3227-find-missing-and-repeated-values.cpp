1b6e7817ac863122344e41a2d1ce0a459afcf3f5
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-18 17:48:03 +0700

    Time: 0 ms (100%), Space: 26.6 MB (77.12%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.cpp
@@ -1,19 +1,46 @@
+/*
+
+How to solve the problem : 
+1. initialize bucket array with size n^2 and default value = 0
+2. loop for every number in grid array
+3. put those numbers in bucket array
+   - bucket[number - 1]++;
+4. loop from first index of bucket array until last index of bucket array
+   - if bucket[i] == 0, that means i + 1 is missing
+   - if bucket[i] == 2, that means i + 1 repeated
+
+Time Complexity : O(N^2)
+N -> size of grid
+
+Memory Complexity : O(N^2)
+N -> size of grid / bucket array
+
+*/
+
+
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        vector<int> bucket((grid.size() * grid.size()) + 1, 0);
-        int repeated = -1;
-        int missing = -1;
+        int size = grid.size();
+        vector<int> bucket(size * size, 0);
+
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[i].size(); j++) {
                 int number = grid[i][j];
-                bucket[number]++;
+
+                // because index start from 0
+                bucket[number - 1]++;
             }
         }
 
+        int missing = -1;
+        int repeated = -1;
         for (int i = 0; i < bucket.size(); i++) {
-            if (bucket[i] == 0) missing = i;
-            else if (bucket[i] == 2) repeated = i;
+            if (bucket[i] == 0) {
+                missing = i + 1;
+            } else if (bucket[i] == 2) {
+                repeated = i + 1;
+            }
         }
 
         return {repeated, missing};
