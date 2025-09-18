e45837558118b3d248d928fd2c7da2658fa15fb9
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-18 20:24:26 +0700

    Time: 0 ms (100%), Space: 8.8 MB (70.18%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.cpp
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -1,23 +1,59 @@
+/*
+
+How to solve the problem : 
+1. Mapping pattern[i] to the word in string s, and put that into the hashtable
+2. if pattern[i] is already have a value, and the value of pattern[i] in hashtable different with word from string s, then return false
+
+Time Complexity : O(N + M)
+N -> length of pattern string
+M -> length of pattern s
+
+Memory Complexity : O(N)
+N -> length of pattern string
+
+*/
+
 class Solution {
 public:
-    bool wordPattern(const string& pattern, const string& str) {
-    unordered_map<char, int> charIndex;
-    unordered_map<string, int> wordIndex;
-    istringstream stream(str);
-    
-    string word;
-    int index = 0;
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string> hashTable;
+        unordered_map<string, bool> visited;
 
-    for (; stream >> word; ++index) {
-        if (index >= pattern.size()) return false;
+        int indexS = 0;
+        for (int i = 0; i < pattern.length(); i++) {
 
-        char c = pattern[index];
-        if (charIndex[c] != wordIndex[word])
+            // get the word from string s
+            string word = "";
+            while (indexS < s.length() && s[indexS] != ' ') {
+                word += s[indexS];
+                indexS++;
+            }
+
+            indexS++;
+
+            if (indexS >= s.length() && i < pattern.length() - 1) {
+                return false;
+            }
+
+            if (hashTable.count(pattern[i]) > 0) {
+                if (hashTable[pattern[i]] != word) {
+                    return false;
+                }
+            } else {
+
+                if (visited[word] == true) {
+                    return false;
+                }
+
+                hashTable[pattern[i]] = word;
+                visited[word] = true;
+            }
+        }
+
+        if (indexS < s.length()) {
             return false;
+        }
 
-        charIndex[c] = wordIndex[word] = index + 1;
+        return true;
     }
-
-    return index == pattern.size();
-}
 };
