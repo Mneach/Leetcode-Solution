6d271b6166be0f3aa77f738ea651b409d9a2be5a
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-19 10:58:44 +0700

    Time: 0 ms (100%), Space: 14 MB (78.8%) - LeetHub

--- 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/1510-find-lucky-integer-in-an-array.cpp
@@ -1,14 +1,28 @@
+/*
+
+1. Put all of those numbers in the hashtable with key = number and value = how many times that number appears in nums of array
+2. Loop for every data in hashtable, and get the largest lucky integer
+
+Time Complexity : O(N + M)
+N -> size of array
+M -> size of hashtable
+
+Memory Complexity : O(M)
+M -> size of hashtable
+
+*/
+
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
-        unordered_map<int,int> bucket;
+        unordered_map<int,int> hashTable;
+        int answer = -1;
 
-        for (auto num : arr) {
-            bucket[num]++;
+        for (auto number : arr) {
+            hashTable[number]++;
         }
 
-        int answer = -1;
-        for (auto data : bucket) {
+        for (auto data : hashTable) {
             int key = data.first;
             int value = data.second;
 
