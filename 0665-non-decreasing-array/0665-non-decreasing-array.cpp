cafc2bc2311bfa376e20b00f04cc44ec0dd1a43c
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-19 16:48:51 +0700

    Time: 0 ms (100%), Space: 30.8 MB (70.61%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.cpp
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -4,7 +4,11 @@ How to solve the problem
 
 # Using greedy approach
 1. Loop from index 1 until the last index of nums
-2. if decreasing value happen more than 1 time return false
+2. if we meet decreasing value, then there will be 2 possibility, either update the current value to become the previous value (nums[i] = nums[i - 1]) or update the prev value to become the first value
+   - if it is happen for the first time, we need to check what if we change the previous value to become the current value 
+     - loop from the previous index until index 1 to make sure the array is valid after we change nums[i - 1] to become nums[i]
+     - if it is valid, then the next time we meet decreasing value, we should return false. Because we only have 1 opportunity to change the number
+     - if it is not valid, then we need to change the current value to become the previous value, then the next time we meet decreasing value, we should return false
 
 Time Complexity : O(N)
 N -> total number in nums array
