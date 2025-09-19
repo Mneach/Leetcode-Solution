4e1636e346bcb7450905dcfd354eecaa3ce6b4b4
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-19 07:36:10 +0700

    Time: 0 ms (100%), Space: 11 MB (95.46%) - LeetHub

--- 1137-height-checker/1137-height-checker.cpp
+++ 1137-height-checker/1137-height-checker.cpp
@@ -1,17 +1,38 @@
+/*
+
+How to solve the problem : 
+1. create another array that contain all of the numbers in heights array
+2. sort the new array asc order
+3. loop from the first index of heights until last index of heights, compare new array[i] with the heights[i], if the result is not equal, then increase the result value by 1
+
+Time Complexity : O(N log N + N)
+N -> size of heights / new array
+N log N -> sorting algorithm time complexity
+last N -> loop from first index of heights until last index of heights
+
+Memory Complexity : O(N)
+N -> size of new array
+
+*/
+
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        
-        vector<int> temp = heights;
-        
-        sort(temp.begin(), temp.end());
-        
-        int answer = 0;
-        
-        for(int i = 0; i < temp.size(); i++){
-            if(temp[i] != heights[i]) answer++;    
+        int result = 0;
+        vector<int> newArray;
+
+        for (auto height : heights) {
+            newArray.push_back(height);
         }
-        
-        return answer;
+
+        sort(newArray.begin(), newArray.end());
+
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != newArray[i]) {
+                result++;
+            }
+        }
+
+        return result;
     }
 };
