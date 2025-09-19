7334a34812cc8c258a9c3049a5eb90451444f97b
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-19 13:59:00 +0700

    Time: 0 ms (100%), Space: 27.7 MB (25.9%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -1,15 +1,25 @@
+/*
+
+How to solve the problem : 
+1. Loop from index 1 until index nums.size() - 1
+2. for every process we need to check if nums[i] % 2 == nums[i - 1] % 2, then return false
+
+Time Complexity : O(N)
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     bool isArraySpecial(vector<int>& nums) {
-        
-        for(int i = 0; i < nums.size() - 1; i++){
-            if(nums[i] % 2 == 0 && nums[i + 1] % 2 == 0){
-                return false;
-            }else if(nums[i] % 2 != 0 && nums[i + 1] % 2 != 0){
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] % 2 == nums[i - 1] % 2) {
                 return false;
             }
         }
-        
+
         return true;
     }
 };
