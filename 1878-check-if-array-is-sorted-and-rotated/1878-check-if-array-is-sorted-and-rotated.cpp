c00fa834302bcab5ed81b394dd341546c98306b6
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-20 09:46:27 +0700

    Time: 0 ms (100%), Space: 11.2 MB (45.92%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,19 +1,46 @@
+/*
+
+How to solve the problem 
+1. Find decreasing value in array nums
+2. start from that index until current decreasing value index - 1
+3. if there is another decreasing value return false
+
+Time Complexity : O(N + N)
+N -> size of nums
+N + N -> since we need to loop 2 times for every number in nums array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        if (nums.size() == 2) return true;
-
-        int totalNotValid = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            int prev = i == 0 ? nums.size() - 1 : i - 1;
-            int current = i;
-            int next = i == nums.size() - 1 ? 0 : i + 1;
-            
-            if (nums[current] >= nums[prev] && nums[next] >= nums[current]) continue;
-            totalNotValid++;
+        
+        // find decreasing value
+        int index = -1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) {
+                index = i;
+                break;
+            }
         }
 
-        if (totalNotValid > 2) return false;
-        else return true;
+        if (index == -1) {
+            return true;
+        }
+
+        int start = (index + 1) % nums.size();
+
+        while (start != index) {
+            if (start == 0 && nums[start] < nums[nums.size() - 1]) {
+                return false;
+            } else if (start > 0 && nums[start] < nums[start - 1]) {
+                return false;
+            }
+            start = (start + 1) % nums.size();
+        }
+
+        return true;
     }
 };
