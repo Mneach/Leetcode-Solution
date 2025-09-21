84d26ce336ad5920e246700889c51da6d9ac3c75
Fitrayuda <90248869+Mneach@users.noreply.github.com>
2025-09-21 19:41:30 +0700

    Time: 0 ms (100%), Space: 111.3 MB (73.16%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -1,24 +1,37 @@
+/*
+
+How to solve the problem : 
+1. loop from first index of the array until last index of the array
+2. we need to get the subarray with value 0
+3. to calculate how many combination that we can create, we can use this formula : (N * (N + 1)) / 2
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
-
-    long long calculate(long long total) {
-        return (total * (total + 1)) / 2;
-    }
-
     long long zeroFilledSubarray(vector<int>& nums) {
         long long answer = 0;
-        long long count = 0; 
+        int count = 0;
+
         for (int i = 0; i < nums.size(); i++) {
+            
             if (nums[i] == 0) {
                 count++;
             } else {
-                answer += calculate(count);
+                long long totalCombination = (1LL * count * (count + 1)) / 2;
+                answer += totalCombination;
                 count = 0;
             }
         }
 
         if (count != 0) {
-            answer += calculate(count);
+            long long totalCombination = (1LL * count * (count + 1)) / 2;
+            answer += totalCombination;
         }
 
         return answer;
