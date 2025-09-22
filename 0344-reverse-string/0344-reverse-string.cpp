809dc0576bb2b11da0a8d891042308c59641e0b8
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-22 14:27:27 +0700

    Time: 0 ms (100%), Space: 27.2 MB (51.36%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -1,9 +1,30 @@
+/*
+
+How to solve the problem : 
+
+# Using Two Pointer Technique
+1. Initiallize left pointer and right pointer
+2. left = 0 and right = s.lenght () -1; 
+3. looping while left < right, then swap s[left] with s[right]
+4. increase left pointer and decrease right pointer by 1
+
+Time Complexity : O(N)
+N -> size of S
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     void reverseString(vector<char>& s) {
-        
-        for(int i = 0; i < s.size() / 2; i++){
-            swap(s[i], s[s.size() - i - 1]);
+        int left = 0;        
+        int right = s.size() - 1;
+
+        while (left < right) {
+            swap(s[left], s[right]);
+            left++;
+            right--;
         }
     }
 };
