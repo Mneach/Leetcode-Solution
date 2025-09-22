f6b3d5a2b95ab0751208f1d111f8f486abe25b2d
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-22 08:21:05 +0700

    Time: 0 ms (100%), Space: 9.8 MB (79.05%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,24 +1,64 @@
+/*
+
+How to solve the problem :
+
+# Using two pointer technique
+1. intialize the left pointer and right pointer
+2. left = 0 and right = s.length() - 1 (last index of string s)
+3. loop from first index until left > right
+4. if s[left] is not a letter, just increaes the left pointer by 1
+5. if s[right] is not a letter, just decrease the right pointer by 1
+6. if (left < right)
+   - if s[left] is not a lowercase letter, then convert to lower case
+   - if s[right] is not a lowercase letter, then covnert to the lowercase
+   - if s[left] != [right] return false
+
+Time Complexity : O(N)
+N -> length of string
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
+private:
+    bool isLetter(char x) {
+        return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z') || (x >= '0' && x <= '9');
+    }
 public:
     bool isPalindrome(string s) {
-        int i = 0;
-        int j = s.size() - 1;
+        int left = 0;
+        int right = s.length() - 1;
 
-        while(i < j){
-
-            while(isalnum(s[i]) == false && i < j) i++;
-            while(isalnum(s[j]) == false && j > i) j--;
+        while (left < right) {
             
-            if(tolower(s[i]) != tolower(s[j]) ){
-                
-                 
-                 return false;
+            while (left < s.length() && isLetter(s[left]) == false) {
+                left++;
             }
 
-            i++;
-            j--;
+            while (right >= 0 && isLetter(s[right]) == false) {
+                right--;
+            }
+
+            if (left < right) {
+
+                if (s[left] >= 'A' && s[left] <= 'Z') {
+                    s[left] = tolower(s[left]);
+                } 
+
+                if (s[right] >= 'A' && s[right] <= 'Z') {
+                    s[right] = tolower(s[right]);
+                }
+
+                if (s[left] != s[right]) {
+                    return false;
+                }
+            }
+
+            left++;
+            right--;
         }
 
-        return true;    
+        return true;
     }
 };
