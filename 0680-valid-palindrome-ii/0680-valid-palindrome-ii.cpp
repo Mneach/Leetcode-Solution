2f481d0f0c31cc41c36272843457834db0741989
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-22 14:25:07 +0700

    Time: 0 ms (100%), Space: 22 MB (81.57%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -1,37 +1,72 @@
+/*
+
+How to solve the problem : 
+
+# Using Two Pointer Technique
+1. initialize left pointer and right pointer
+2. left = 0 and right = s.length() - 1
+3. if (s[left] != s[right]), then there will be 2 possiblity
+   - try to skip the left, by add left pointer by 1
+   - try to skip the right, by decrease right pointer by 1
+   - mark if the result is not valid, and return false
+
+Time Complexity : O(N)
+N -> size of string s
+
+Memory Compleixty : O(1)
+
+*/
+
 class Solution {
 public:
     bool validPalindrome(string s) {
-        
         int left = 0;
         int right = s.length() - 1;
 
         while (left < right) {
             if (s[left] != s[right]) {
-                int leftNext = left + 1;
-                int rightNext = right  - 1;
-
-                while (leftNext < right) {
-                    if (s[leftNext] == s[right]) {
-                        leftNext++;
-                        right--;
-                    } else {
-                        break;
-                    }
-                }
-
-                while (left < rightNext) {
-                    if (s[left] == s[rightNext]) {
-                        rightNext--;
-                        left++;
-                    } else {
-                        break;
-                    }
-                }
-
-                return left >= rightNext || leftNext >= right;
+                break;
             }
             left++;
             right--;
+        }
+
+        if (left < right) {
+            int tempLeft = left + 1;
+            int tempRight = right;
+            bool valid = true;
+
+            while (tempLeft < tempRight) {
+                if (s[tempLeft] != s[tempRight]) {
+                    valid = false;
+                    break;
+                }
+
+                tempLeft++;
+                tempRight--;
+            }
+
+            if (valid) {
+                return true;
+            }
+
+            valid = true;
+            tempLeft = left;
+            tempRight = right - 1;
+            while (tempLeft < tempRight) {
+                if (s[tempLeft] != s[tempRight]) {
+                    return false;
+                }
+
+                tempLeft++;
+                tempRight--;
+            }
+
+            if (valid) {
+                return true;
+            }
+
+            return false;
         }
 
         return true;
