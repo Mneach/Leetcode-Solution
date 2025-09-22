68f4d939d4c2c8073253ace9120643dd7c153a44
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-22 08:04:05 +0700

    Time: 27 ms (14.8%), Space: 85.6 MB (61.19%) - LeetHub

--- 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
+++ 2232-adding-spaces-to-a-string/2232-adding-spaces-to-a-string.cpp
@@ -1,20 +1,54 @@
+/*
+
+How to solve the problem : 
+
+# Using Brute Force Solution
+1. For every single character in string s, we need to check if the current index exist in the spaces array, if it is exist, than append the current result with space and s[i]
+
+Time Complexity : O(N * M)
+N -> size of the string
+M -> size of the spaces
+
+# What makes the algorithm run slowly ? 
+1. Because for every index in string s, we need to check if the curret index appear in the spaces array
+
+# How to improve ? 
+1. what if we are using two pointer to maintain the index of string s and the index of array spaces, so we don't need to loop from the first index until the last index in spaces array
+
+# Using two pointer technique
+1. Define left and right pointer with value 0
+2. Left pointer for string s and right pointer for spaces array
+3. loop from the first index of string s until last index of string s
+   - if (right < spaces.size() && left == spaces[right]), then append answer with space + s[left] and increase right pointer by 1
+   - otherwise just append answer with s[left]
+   - increase left pointer by 1
+
+Time Complexity : O(N + M)
+N -> size of string s
+M -> size of the spaces
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     string addSpaces(string s, vector<int>& spaces) {
+        int left = 0;        
+        int right = 0;
         string answer = "";
 
-        unordered_map<int,int> ump;
-        for(auto number : spaces) {
-            ump[number]++;
-        }
+        while (left < s.length()) {
 
-        for (int i = 0; i < s.length(); i++) {
-            
-            if (ump[i] > 0) {
-                answer += " ";
+            if (right < spaces.size() && left == spaces[right]) {
+                answer += ' ';
+                answer += s[left];
+                right++;
+            } else {
+                answer += s[left];
             }
 
-            answer += s[i];
+            left++;
         }
 
         return answer;
