7157632bcba7086c135dbdd8ec84b513ae06c550
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-22 17:59:22 +0700

    Time: 0 ms (100%), Space: 12.6 MB (96.55%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,45 +1,102 @@
-class Solution {
-public:
-    
-    int compress(vector<char>& chars) {
-        
-        vector<char> answer;
-        
-        int currentChar = chars[0];
-        int currentTotal = 1;
-        
-        for(int i = 1; i < chars.size(); i++){
-            if(currentChar != chars[i]){
-                answer.push_back(currentChar);
-                
-                if(currentTotal > 1){
-                    string s = to_string(currentTotal);
-                    
-                    for(int j = 0; j < s.length(); j++){
-                        answer.push_back(s[j]);
-                    }
-                }
-                
-                currentChar = chars[i];
-                currentTotal = 1;
-            }else{
-                currentTotal++;
-            }
-        }
-        
-        answer.push_back(currentChar);
-        if(currentTotal > 1){
-            string s = to_string(currentTotal);
+/*
 
-            for(int j = 0; j < s.length(); j++){
-                answer.push_back(s[j]);
-            }
-        }        
-        
-        for(int i = 0; i < answer.size(); i++){
-            chars[i] = answer[i];
+How to solve the problem 
+
+# Using Two Pointer 
+1. initialize the left pointer and right pointer
+2. left = 0 and right = 0
+3. Loop from the first index of chars array until last index of chars array
+4. If the chars[right] == chars[right + 1]
+   - increase count variable by  1
+3. else
+   - count how many digit that we have
+   - loop from left pointer + total digit until left pointer + 1
+     - divide the count by 10 to get the digit
+     - nums[currentIndex] = digit
+     - currentIndex--;
+   - count = 0
+   - add answer by current digit value
+4. return the answer
+
+Time Complexity : O(N + M)
+N -> size of chars array
+M -> maximum digit from count variable value
+
+Memory Complexity : O(1)
+
+
+*/
+
+class Solution {
+private:
+    int countDigit(int number) {
+        int result = 0;
+
+        while (number > 0) {
+            number /= 10;
+            result++;
         }
-        
-        return chars.size() - (chars.size() - answer.size());
+
+        return result;
+    }
+public:
+    int compress(vector<char>& chars) {
+        int count = 1;
+        int left = 0;
+        int right = 0;
+
+        while (right < chars.size() - 1) {
+            if (chars[right] == chars[right + 1]) {
+                count++;
+            } else {
+                if (count > 1) {
+                    char currentChar = chars[right];
+                    chars[left] = currentChar;
+
+                    int totalDigit = countDigit(count);
+                    int currentIndex = left + totalDigit;
+                    while (currentIndex > left) {
+                        // take the digit
+                        int digit = count % 10;
+                        chars[currentIndex] = digit + '0';
+                        currentIndex--;
+                        count /= 10;
+                    }
+
+                    left = left + totalDigit + 1;
+                } else if (count <= 1) {
+                    char currentChar = chars[right];
+                    chars[left] = currentChar;
+                    left++;
+                } 
+
+                count = 1;
+            }
+
+            right++;
+        }
+
+        if (count <= 1) {
+            char currentChar = chars[chars.size() - 1];
+            chars[left] = currentChar;
+            left++;
+        } else {
+            char currentChar = chars[chars.size() - 1];
+            chars[left] = currentChar;
+
+            int totalDigit = countDigit(count);
+            int currentIndex = left + totalDigit;
+            while (currentIndex > left) {
+                // take the digit
+                int digit = count % 10;
+                chars[currentIndex] = digit + '0';
+                currentIndex--;
+                count /= 10;
+            }
+            
+            left = left + totalDigit + 1;
+        }
+
+        return left;
     }
 };
