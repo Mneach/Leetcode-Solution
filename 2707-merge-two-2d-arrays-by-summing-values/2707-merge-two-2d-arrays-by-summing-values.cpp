898802aa3432deab0ba4f507cee21337175e4032
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-23 14:51:10 +0700

    Time: 0 ms (100%), Space: 14.6 MB (96.46%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.cpp
@@ -1,29 +1,62 @@
+/*
+
+How to solve the problem 
+
+# Using two pointer technique
+1. initialize left and right pointer
+2. left = 0 and right = 0
+3. if left id > right id
+   - push nums2[right] to the answer
+   - increase right pointer by 1 
+4. if left id < right id
+   - push nums1[left] to the answer
+   - incrase right pointer by 1
+5. if left id == right id, 
+   - then add nums1[left] value by nums2[right]
+   - add nums1[left] to the answer
+   - increase left and right pointer by 1
+
+Time Complexity : O(N + M)
+N -> size of nums1
+M -> size of nums2
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
-        unordered_map<int,int> ump;
+        vector<vector<int>> answers;
+        int left = 0;
+        int right = 0;
 
-        for (auto data : nums1) {
-            int id = data[0];
-            int value = data[1];
+        while (left < nums1.size() && right < nums2.size()) {
 
-            ump[id] += value;
+            if (nums1[left][0] > nums2[right][0]) {
+                answers.push_back(nums2[right]);
+                right++;
+            } else if (nums1[left][0] < nums2[right][0]) {
+                answers.push_back(nums1[left]);
+                left++;
+            } else {
+                nums1[left][1] += nums2[right][1];
+                answers.push_back(nums1[left]);
+                left++;
+                right++;
+            }
         }
 
-        for (auto data : nums2) {
-            int id = data[0];
-            int value = data[1];
-
-            ump[id] += value;
+        while (left < nums1.size()) {
+            answers.push_back(nums1[left]);
+            left++;
         }
 
-        vector<vector<int>> answer;
-        for (auto data : ump) {
-            answer.push_back({data.first, data.second});
+        while (right < nums2.size()) {
+            answers.push_back(nums2[right]);
+            right++;
         }
 
-        sort(answer.begin(),answer.end());
-
-        return answer;
+        return answers;
     }
 };
