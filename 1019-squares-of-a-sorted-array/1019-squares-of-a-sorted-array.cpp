81136ffbd3d75c792ef2435824eccbe60b542506
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-24 12:28:16 +0700

    Time: 0 ms (100%), Space: 30.3 MB (37.2%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.cpp
@@ -11,17 +11,44 @@ N -> size of nums
 
 Memory Complexity : O(1)
 
+# Using Two Pointer 
+1. Initialize left and right pointer
+2. left = 0 and right = nums.size() - 1;
+3. compare nums[i] ^ 2 with nums[right] ^ 2, put the highest value at end index of answer array
+4. if nums[i]^2 > nums[right]^2, then increase the left pointer
+5. else decrease the right pointer
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(N)
+N -> size of nums
+
 */
 
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] *= nums[i];
+        vector<int> answers(nums.size(), 0);
+        int left = 0;
+        int right = nums.size() - 1;
+        int answerIndex = nums.size() - 1;
+
+        while (left <= right) {
+            long long leftResult = nums[left] * 1LL * nums[left];
+            long long rightResult = nums[right] * 1LL * nums[right];
+
+            if (leftResult > rightResult) {
+                answers[answerIndex] = leftResult;
+                left++;
+            } else {
+                answers[answerIndex] = rightResult;
+                right--;
+            }
+
+            answerIndex--;
         }
 
-        sort(nums.begin(), nums.end());
-
-        return nums;
+        return answers;
     }
 };
