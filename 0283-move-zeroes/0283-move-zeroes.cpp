b98b0034d4eb83ffd53ab34007c6b7bb2f3d17ce
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-24 11:37:42 +0700

    Time: 0 ms (100%), Space: 23.9 MB (55.52%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,12 +1,42 @@
+/*
+
+How to solve the problem : 
+
+# Using Two Pointer Technique
+1. Initialize left and right pointer
+2. left = 0, right = 0
+3. looping until right >= nums.size()
+4. if nums[right] != 0
+   - move left pointer to nums[left] == 0, and make sure left < right
+   - if nums[left] == 0, swam nums[left] to nums[right]
+5. increase right pointer by 1
+
+Time Copmlexity : O(N)
+N -> size of nums
+
+Memory Compelixty : O(1)
+
+
+*/
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        int currentIndexZero = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] != 0){
-                swap(nums[currentIndexZero++], nums[i]);
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.size()) {
+            if (nums[right] != 0) {
+                while(left < right && nums[left] != 0) {
+                    left++;
+                }
+
+                if (nums[left] == 0) {
+                    swap(nums[left], nums[right]);
+                    left++;
+                }
             }
+            right++;
         }
     }
 };
