eef665280e2b99d2daa0e8830643c9a1d2782311
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-24 17:34:25 +0700

    Time: 11 ms (42.14%), Space: 44.7 MB (74.11%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.cpp
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -1,19 +1,43 @@
+/*
+
+How to solve the problem
+
+# Using Two pointer + Sorting
+1. Sort the g and s arrays
+2. intialize left and right pointer (left = 0 and right = 0)
+3. while g[left] < s[right]
+   - increase left pointer by 1
+   - increase right pointer by 1
+4. return the left pointer
+
+Time Complexity : O(N log N + M log M)
+N -> size of g arrays
+M -> size of s arrays
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        
-        sort(g.begin(), g.end());  
+        int left = 0;
+        int right = 0;
+        int answer = 0;
+
+        sort(g.begin(), g.end());
         sort(s.begin(), s.end());
-        
-        int contentChildren = 0;
-        int cookieIndex = 0;
-        
-        while(cookieIndex < s.size() && contentChildren < g.size()){
-            if(s[cookieIndex] >= g[contentChildren]){
-                contentChildren++;
+
+        while (left < g.size() && right < s.size()) {
+            if (g[left] <= s[right]) {
+                left++;
+                right++;
+                answer++;
+            } else if (g[left] > s[right]) {
+                right++;
             }
-            cookieIndex++;
         }
-        return contentChildren;
+        
+        return answer;
     }
 };
