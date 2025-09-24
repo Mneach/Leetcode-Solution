cde8c58ebeb2a0b68d59e37367e673171e99946f
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-24 13:16:13 +0700

    Time: 0 ms (100%), Space: 63 MB (12.47%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,25 +1,47 @@
+/*
+
+How to solve the problem 
+
+# Using Two Pointer Technique
+1. Initialize left and right pointer
+2. left = 0 and right = height.size() - 1
+3. while left < right
+   - take the minimum height between heigh[left] and height[right]
+   - calculate the distance between those indexes using this formula : right - left
+   - calculate the current answer using this formula : minHeight * distance
+   - compare that current answer to the real answer variable
+   - if nums[left] > nums[right], decrease the right pointer by 1  
+   - else if nums[left] < nums[right], increase left pointer by 1
+
+Time Complexity : O(N)
+N -> size of height array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        
-        int size = height.size() - 1;
         int left = 0;
+        int right = height.size() - 1;
         int answer = 0;
-        int right = size;
-        int currentSize = size;
-        
-        while(left < right){
-            answer = max(answer, min(height[left], height[right]) * currentSize);
-            
-            // printf("%d %d %d %d\n", height[left], height[right], currentSize, answer);
-            if(height[left] < height[right]){
-                left++;
-            }else{
+
+        while (left < right) {
+            int minimumHeight = min(height[left], height[right]);
+            int distance = right - left;
+            int currentAnswer = minimumHeight * distance;
+            answer = max(answer, currentAnswer);
+
+            if (height[left] > height[right]) {
                 right--;
-            }          
-            currentSize--;
+            } else if (height[left] < height[right]) {
+                left++;
+            } else {
+                left++;
+            }
         }
-        
+
         return answer;
     }
 };
