e6b501cf916718ba3a078767b23bbec9a21af3d6
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-25 21:38:20 +0700

    Time: 0 ms (100%), Space: 12 MB (90.7%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -1,28 +1,47 @@
+/*
+
+How to solve the problem
+
+1. Loop from first index of string s until last index of string s
+2. Put the left pointer at the left of word and right pointer at the right of word
+3. Swap left pointer and right pointer
+4. increaes left pointer and decrease right pointer
+
+Time Complexity : O(N)
+N -> size of string s
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     string reverseWords(string s) {
-      
-        int size = s.length();
-        
         int left = 0;
-        for(int i = 0; i < size; i++){
-            if(s[i] == ' ' || i == size - 1){
-                int right;
-                
-                if(i == size - 1) right = i;
-                else right = i - 1;
-                
-                while(left < right){
-                    swap(s[left], s[right]);
+        int right = 0;
+
+        while (right < s.length()) {
+            if (s[right] == ' ') {
+                int temp = right - 1;
+
+                while (left < temp) {
+                    swap(s[left], s[temp]);
                     left++;
-                    right--;
-                } 
-                
-                left = i + 1;
+                    temp--;
+                }
+
+                left = right + 1;
             }
-        } 
-        
-        
+
+            right++;
+        }
+
+        while (left < right) {
+            right--;
+            swap(s[left], s[right]);
+            left++;
+        }
+
         return s;
     }
 };
