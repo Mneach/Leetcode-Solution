967bbdd546d0ee16091e7e4b4970ac96227e757a
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-26 08:44:21 +0700

    Time: 0 ms (100%), Space: 127.8 MB (59.37%) - LeetHub

--- 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
+++ 2085-array-with-elements-not-equal-to-average-of-neighbors/2085-array-with-elements-not-equal-to-average-of-neighbors.cpp
@@ -21,30 +21,42 @@ N / 2 -> to get the answer
 Memory Complextiy : O(M)
 M -> size of answers array
 
+# What makes the algorithm run slowly ? 
+1. is because of we need to sort the nums array
+
+# How to improve the algorithm 
+1. There is a pattern to solve this problem, we should create nums[i] > nums[i - 1] and nums[i] > nums[i + 1] or nums[i] < nums[i - 1] and nums[i] < nums[i + 1]
+   - The reason is because if the neighboar is less than nums[i], that means sum of those neighboar must be less than nums[i] * 2
+   - The reason is because if the neighboar is more than nums[i], that means sum of those neightboar must be more than nums[i] * 2
+
+# Greedy 
+1. Create a increase flag, by comparing nums[1] with nums[0]
+2. If the current flag is increase and nums[i] < nums[i + 1], we need to swap nums[i] with nums[i + 1], so we can create the pattern like this (SLS), then update the flag to decrease. S = small and L = large
+3. If the current flag is decrease and nums[i] > nums[i + 1], we neeed to swap nums[i] with nums[i + 1], so we can create the pattern like this (LSL), then update the flag to increase
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
 */
 
 class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
-        vector<int> answers(nums.size(), 0);
+        bool isIncrease = nums[0] < nums[1];
 
-        sort(nums.begin(), nums.end());
-        int left = 0;
-        int right = nums.size() - 1;
-        int index = 0;
-
-        while (left <= right) {
-            if (left != right) {
-                answers[index] = nums[right];
-                answers[index + 1] = nums[left];
-                index += 2;
-            } else {
-                answers[index] = nums[left];
+        for (int i = 1; i < nums.size() - 1; i++) {
+            
+            if (isIncrease && nums[i] < nums[i + 1]) {
+                swap(nums[i], nums[i + 1]);
+            } else if (!isIncrease && nums[i] > nums[i + 1]) {
+                swap(nums[i], nums[i + 1]);
             }
-            left++;
-            right--;
+
+            isIncrease = !isIncrease;
         }
 
-        return answers;
+        return nums;
     }
 };
