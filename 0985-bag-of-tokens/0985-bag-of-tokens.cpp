0971892743b20514c8b59d10b502ea304ecd7ac0
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-26 16:18:06 +0700

    Time: 3 ms (20.34%), Space: 14.4 MB (11.14%) - LeetHub

--- 0985-bag-of-tokens/0985-bag-of-tokens.cpp
+++ 0985-bag-of-tokens/0985-bag-of-tokens.cpp
@@ -6,11 +6,11 @@ How to solve the problem
 1. Sort the tokens
 2. initialize two pointer (left = 0 and right = tokens.size() - 1)
 3. while (left <= right)
-    - while power > tokens[left]
+    - if power > tokens[left]
       - decrease the power by tokens[left]
       - increase the score by 1
       - increase left pointer by 1
-    - if current score > 0
+    - else if current score > 0
       - get the maximum answer -> max(answer, currentScore);
       - increaes the power by the tokens[right]
       - decrease current score by 1
@@ -32,22 +32,16 @@ public:
         sort(tokens.begin(), tokens.end());
 
         while (left <= right) {
-            while (left <= right && power >= tokens[left]) {
+            if (power >= tokens[left]) {
                 power -= tokens[left];
-                left++;
-                currentScore++;
-            }
-
-            if (currentScore > 0) {
+                currentScore += 1;
                 answer = max(answer, currentScore);
+                left++;
+            } else if (currentScore > 0) {
                 power += tokens[right];
                 currentScore--;
                 right--;
-            }
-
-            if (left > right) {
-                break;
-            } else if (power < tokens[left] && currentScore == 0) {
+            } else {
                 break;
             }
         }
