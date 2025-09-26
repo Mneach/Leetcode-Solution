9e791cfce4bad2f01e870609b60fa7969c56016e
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-26 12:42:00 +0700

    Time: 0 ms (100%), Space: 20.6 MB (70.72%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,8 +1,41 @@
+/*
+
+How to solve the problem 
+
+# Using Two Pointer Technique
+1. Initialize left and right pointer (left = 0 and right = 0)
+2. while right < nums.size()
+   - while left < right && nums[left] % 2 == 0
+     - increase left pointer by 1
+   - if (nums[left] % 2 == 1 && nums[right] % 2 == 1) 
+     - swap nums[left] with nums[right]
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     vector<int> sortArrayByParity(vector<int>& nums) {
-         for (int i = 0, j = 0; j < nums.size(); j++)
-            if (nums[j] % 2 == 0) swap(nums[i++], nums[j]);
+        int left = 0;
+        int right = 0;
+
+        while (right < nums.size()) {
+
+            while (left < right && nums[left] % 2 == 0) {
+                left++;
+            } 
+
+            if (nums[left] % 2 == 1 && nums[right] % 2 == 0) {
+                swap(nums[left], nums[right]);
+            }
+
+            right++;
+        }
+
         return nums;
     }
 };
