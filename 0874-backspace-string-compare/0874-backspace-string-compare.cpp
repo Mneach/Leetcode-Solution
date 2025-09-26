6b9245146bc290858f03118cc76466e15afd7284
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-09-26 17:02:38 +0700

    Time: 0 ms (100%), Space: 8.4 MB (71.47%) - LeetHub

--- 0874-backspace-string-compare/0874-backspace-string-compare.cpp
+++ 0874-backspace-string-compare/0874-backspace-string-compare.cpp
@@ -1,32 +1,75 @@
+/*
+
+How to solve the problem
+
+# Using two pointer techcnique
+1. Initialize left and right pointer (left = s.length() - 1 && right = t.length() - 1)
+2. while left >= 0 && right >= 0
+   - find next valid character for string s
+   - find next valid character for string t
+   - if charS != charT return false
+   - decrease left index by 1
+   - decrease right index by 1
+
+Time Complexity : O(N + M)
+N -> size of s
+M -> size of t
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
+private:
+    int findValidNextChar(string &s, int currentIndex) {
+        int backSpace = 0;
+
+        while (currentIndex >= 0) {
+            if (s[currentIndex] == '#') {
+                backSpace++;
+            } else if (backSpace > 0) {
+                backSpace--;
+            } else {
+                break;
+            }
+
+            currentIndex--;
+        }
+
+        return currentIndex;
+    }
 public:
     bool backspaceCompare(string s, string t) {
-        stack<char> x;
-        stack<char> y;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] == '#'  && x.empty() == false) x.pop();
-            else x.push(s[i]);
+        int left = s.length() - 1;
+        int right = t.length() - 1;
+
+        while (left >= 0 || right >= 0) {
+            int indexS = findValidNextChar(s, left);
+            int indexT = findValidNextChar(t, right);
+
+            char charS;
+            char charT;
+
+            if (indexS < 0) {
+                charS = '\0';
+            } else {
+                charS = s[indexS];
+            }
+
+            if (indexT < 0) {
+                charT = '\0';
+            } else {
+                charT = t[indexT];
+            }
+
+            if (charS != charT) {
+                return false;
+            }
+
+            left = indexS - 1;
+            right = indexT - 1;
         }
-        
-        for(int i = 0; i < t.length(); i++){
-            if(t[i] == '#' && y.empty() == false) y.pop();
-            else y.push(t[i]);            
-        }
-        
-        string resultX = "";
-        string resultY = "";
-        
-        while(x.empty() == false) {
-            if(x.top() != '#') resultX += x.top();
-            x.pop();
-        }
-        
-        while(y.empty() == false){
-            if(y.top() != '#') resultY += y.top();
-            y.pop();
-        }
-        
-        return resultX == resultY;
+
+        return true;
     }
 };
