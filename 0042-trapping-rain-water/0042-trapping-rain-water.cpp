36e0326e93ee9fa2cc936dfb85cfe662062af99f
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-01 20:04:39 +0700

    Time: 303 ms (5.25%), Space: 26 MB (60.6%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -35,6 +35,26 @@ Memory Complexity : O(M + P)
 M -> size of left array
 P -> size of right array
 
+# What makes the solution 2 run slowly and make more space ? 
+1. because we need to precompute the left and right array 
+
+# How to improvel the solution ? 
+1. what if we just need to save the next greater element using two pointer left and right
+
+# Using Two Pointer
+1. loop from first index of height array until last index of height array
+2. if left == right
+   - find next greater right element
+3. if height[i] >= left
+   - update left pointer to i
+4. to computer how many water that we can get, we can use this formula
+   - formula : min(height[left], height[right]) - height[i]
+
+Time Complexity : O(N + N)
+N -> size of height array
+
+Memory Complexity : O(1)
+
 */
 
 class Solution {
@@ -42,23 +62,29 @@ public:
     int trap(vector<int>& height) {
         int answer = 0;
         int size = height.size();
-        vector<int> left(size, 0);
-        vector<int> right(size, 0);
+        int left = 0;
+        int right = 0;
 
         for (int i = 0; i < size; i++) {
 
-            if (i == 0) {
-                left[i] = height[i];
-                right[size - i - 1] = height[size - i - 1];
+            if (height[i] >= height[left]) {
+                left = i;
+            }
+
+            if (i == right) {
+                // find next right greater element
+                right++;
+                for (int j = right + 1; j < size; j++) {
+                    if (height[j] >= height[right]) {
+                        right = j;
+                    }
+                }
+
                 continue;
             }
 
-            left[i] = max(left[i - 1], height[i]);
-            right[size - i - 1]  = max(right[size - i], height[size - i - 1]);
-        }
+            answer += min(height[left], height[right]) - height[i];
 
-        for (int i = 0; i < size; i++) {
-            answer += min(left[i], right[i]) - height[i];
         }
 
         return answer;
