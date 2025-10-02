18458a8368015a25f480f05900f1dde4f966cad4
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-02 13:50:44 +0700

    Time: 75 ms (54.84%), Space: 93.9 MB (74.66%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,24 +1,42 @@
+/*
+
+How to solve the problem
+
+# Using sliding window
+1. loop from first index of nums until last index of nums
+2. if nums[i] already exists in the hashtable
+   - return true
+3. otherwise
+   - put nums[i] in the hashtable
+4. if (i >= k)
+   - erase hashtable with key = nums[i - k]
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(K)
+
+*/
+
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        
-        
-        map<int, vector<int>> mp;
-        
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]].push_back(i);
-        }
-        
-        for(auto data : mp){
-            vector<int> indexs = data.second;
-            
-            for(int i = 0; i < indexs.size() - 1; i++){
-                if(abs(indexs[i] - indexs[i + 1]) <= k){
-                    return true;
-                }
+        unordered_map<int,int> hashTable;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int number = nums[i];
+
+            if (hashTable[number] > 0) {
+                return true;
             }
-        }
-        
+
+            hashTable[number]++;
+
+            if (i >= k) {
+                hashTable.erase(nums[i - k]);
+            }
+        }        
+
         return false;
     }
 };
