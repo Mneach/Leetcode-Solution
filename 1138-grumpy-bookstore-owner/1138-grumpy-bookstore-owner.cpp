2e1f90739f1ff24d7550681d294d30451414a023
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-03 19:46:18 +0700

    Time: 0 ms (100%), Space: 35.9 MB (8.35%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -3,15 +3,13 @@
 How to solve the problem : 
 1. initialize variables (count = 0, result = 0)
 2. loop from the first index of the array until the last index of the array
-3. if grumpy[i] == 0, then add county by customers[i]
-4. after that we try to use the secret technique
-5. loop from the first index of the array until the last index of the array
+   - if grumpy[i] == 0, then add county by customers[i]
    - if grumpy[i] == 1, add count by customers[i]
    - if i >= minutes && grumpy[i - minutes] == 1
      - decrease count by customers[i - minutes] 
    - get the maximum result (result = max(result, count))
 
-Time Complexity : O(N + N)
+Time Complexity : O(N)
 N -> size of grumpy array
 
 Memory Complexity : O(1)
@@ -22,26 +20,24 @@ class Solution {
 public:
     int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
         int count = 0;
-        int result = 0;
+        int secret = 0;
+        int maxSecret = 0;
 
         for (int i = 0; i < grumpy.size(); i++) {
             if (grumpy[i] == 0) {
                 count += customers[i];
-            }
-        }
-
-        for (int i = 0; i < grumpy.size(); i++) {
-            if (grumpy[i] == 1) {
-                count += customers[i];
+            } else {
+                secret += customers[i];
             }
 
             if (i >= minutes && grumpy[i - minutes] == 1) {
-                count -= customers[i - minutes];
+                secret -= customers[i - minutes];
             }
 
-            result = max(result, count);
+            maxSecret = max(secret, maxSecret);
+
         }
 
-        return result;
+        return count + maxSecret;
     }
 };
