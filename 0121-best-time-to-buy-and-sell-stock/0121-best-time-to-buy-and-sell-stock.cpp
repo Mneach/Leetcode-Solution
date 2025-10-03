a68a528bb3d714e7466298d6dd69c9765375cd05
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-04 05:49:52 +0700

    Time: 0 ms (100%), Space: 97.5 MB (6.97%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,42 @@
+/*
+
+How to solve the problem 
+
+# Using two pointer
+1. initialize left pointer, right pointer and result
+2. loop from the first index of prices array until last index of prices array
+   - if (prices[right] < prices[left])
+     - move left pointer to right pointer
+   - else
+     - calculate the result using this formula : prices[right] - prices[left]
+     - get the maximum result (result = max(result, prices[right] - prices[left]))
+   - increase the right pointer by 1
+3. return the result
+
+Time Complexity : O(N)
+N -> size of prices array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        
-        int answer = 0;
-        int minimum = INT_MAX;
-        
-        
-        for(int i = 0 ; i < prices.size(); i++){
-            
-            if(prices[i] <= minimum){
-                minimum = prices[i];
-            }else{
-                answer = max(prices[i] - minimum, answer);
+        int left = 0;
+        int right = 0;
+        int result = 0;
+
+        while (right < prices.size()) {
+            if (prices[left] > prices[right]) {
+                left = right;
+            } else {
+                result = max(result, prices[right] - prices[left]);
             }
+
+            right++;
         }
-        
-        return answer;
+
+        return result;
     }
 };
