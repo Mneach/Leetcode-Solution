781a0ee759002b1fcefb66529c2a77b45eb9b597
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-03 19:41:46 +0700

    Time: 4 ms (16.84%), Space: 35.6 MB (96.58%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.cpp
@@ -2,14 +2,16 @@
 
 How to solve the problem : 
 1. initialize variables (count = 0, result = 0)
-1. Loop from the first index of the array until the last index of the array
-2. add customers[i] to the count variable
-3. if (i >= minutes)
-   - if (grumpy[i - minutes] == 1)
-     - decrease count by customers[i - minutes]
-4. get the maximum count value (result = max(result, count))
+2. loop from the first index of the array until the last index of the array
+3. if grumpy[i] == 0, then add county by customers[i]
+4. after that we try to use the secret technique
+5. loop from the first index of the array until the last index of the array
+   - if grumpy[i] == 1, add count by customers[i]
+   - if i >= minutes && grumpy[i - minutes] == 1
+     - decrease count by customers[i - minutes] 
+   - get the maximum result (result = max(result, count))
 
-Time Complexity : O(N)
+Time Complexity : O(N + N)
 N -> size of grumpy array
 
 Memory Complexity : O(1)
