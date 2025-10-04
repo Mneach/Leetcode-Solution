440f919a2ff1ce0164415be8b0304b9fd549ce0b
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-04 11:39:19 +0700

    Time: 3 ms (81.83%), Space: 11.9 MB (55.94%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -18,7 +18,32 @@ N -> size of string s
 First N -> because we need to loop from first index of s until last index
 Second N -> becase we need to loop from current left pointer until hashTable[s[right]] + 1 index
 
-Memory Complexty : O(1)
+Memory Complexty : O(M)
+M -> total unique characters in hashtable
+
+# What makes the algorithm run slowly ? 
+1. because we need to move the left pointer
+
+# How to improve the problem ? 
+1. instead of moving the left pointer to remove the character from hashtable, it is better to move directly the left pointer to hashTable[s[right]] + 1. The reason is because of after we move the left pointer to hashTable[s[right]] + 1, all of the characters before that is useless
+
+# Using sliding window optimal
+1. Initialize left, right, result variables (left = 0, right = 0, result = 0)
+2. Initialize hashtable -> key = character, value = index
+3. loop from the first index of s until last index of s
+    - if (hashtable.count(s[right]) > 0)
+     - move the left pointer to hashtable[s[right]] + 1
+   - add s[right] into the hashtable
+   - get the maximum result (result = max(result, (right - left) + 1)
+   - increase right pointer by 1
+
+Time Complexity : O(N)
+N -> size of string s
+First N -> because we need to loop from first index of s until last index
+Second N -> becase we need to loop from current left pointer until hashTable[s[right]] + 1 index
+
+Memory Complexty : O(M)
+M -> total unique characters in hashtable
 
 */
 
@@ -32,11 +57,8 @@ public:
 
         while (right < s.length()) {
             if (hashTable.count(s[right]) > 0) {
-                // move left pointer until hashTable[s[right]] + 1 
-                while (left < hashTable[s[right]] + 1) {
-                    hashTable.erase(s[left]);
-                    left++;
-                }
+                // move left pointer until hashTable[s[right]] + 1
+                left = max(hashTable[s[right]] + 1, left);
             }
 
             hashTable[s[right]] = right;
