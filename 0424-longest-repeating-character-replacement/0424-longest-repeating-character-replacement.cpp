e16d67049ee6fb8aa24b0edcb36238e5979f73c4
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-04 14:19:51 +0700

    Time: 13 ms (19.92%), Space: 11 MB (40.27%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,3 +1,30 @@
+/*
+
+How to solve the problem
+
+# Using sliding window
+1. Initialize left, right, bucket of characters, and result
+2. while right < s.length()
+   - get the ascii character, put that into the bucket
+   - get the maximum number and total in the bucket
+   - decrease total by maximum number
+   - while total > k
+     - should move the left pointer and remove the s[left] from the bucket, since this current window is not valid anymore
+     - get the maximum number and total in the bucket
+     - decrease total by maximum number
+   - get the result answer using this formula : result = max(result, right - left + 1)
+3. return result
+
+Time Complexity : O(N * M^2)
+N -> length of string s
+M -> total different characters in the bucket array -> worst case 26 characters
+
+Memory Complexity : O(M)
+M -> total different characters in the bucket array
+
+*/
+
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -32,8 +59,6 @@ public:
                 total -= maximum;
                 left++;
             }
-
-            // cout << left << " " << right << endl;
 
             result = max(result, (right - left) + 1);
             right++;
