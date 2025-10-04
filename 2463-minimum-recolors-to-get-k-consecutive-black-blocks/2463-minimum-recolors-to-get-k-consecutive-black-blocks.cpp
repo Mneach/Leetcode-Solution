f7e8fe80d3f9f548993508feb82e6a4d8cd58a78
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-04 10:47:50 +0700

    Time: 0 ms (100%), Space: 8.3 MB (92.19%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,28 +1,52 @@
+
+/*
+
+How to solve the problem : 
+
+# Using Sliding Window
+1. initialize left, right, count, and result (left = 0, right = 0, count = k, result = k)
+2. loop from the first index of blocks string until the last index of blocks strings
+   - if blocks[right] == 'W'
+     - if (count == k) count = 1
+     - else count++
+   - if (right - left) + 1 == k
+     - result = min(result, count)
+     - if (blocks[left] == 'W') count--
+     - increase left pointer by 1
+   - right++
+
+Time Complexity : O(N)
+N -> length of blocks string
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        int answer = INT_MAX;
-        int totalReplace = 0;
         int left = 0;
+        int right = 0;
+        int count = 0;
+        int result = k;
 
-        for (int right = 0; right < blocks.size(); right++) {
-            char curr = blocks[right];
-            int currentTotal = (right - left) + 1;
-
-            if (curr == 'W') {
-                totalReplace++;
+        while(right < blocks.length()) {
+            if (blocks[right] == 'W') {
+                count++;
             }
 
-            if (currentTotal == k) {
-                answer = min(answer, totalReplace);
+            if ((right - left) + 1 == k) {
+                result = min(result, count);
                 
                 if (blocks[left] == 'W') {
-                    totalReplace--;
+                    count--;
                 }
                 left++;
             }
+
+            right++;
         }
 
-        return answer;
+        return result;
     }
 };
