0d37c8a868e5c76597d01139b2b4f07e8d0922ed
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-05 09:52:33 +0700

    Time: 43 ms (46.22%), Space: 75 MB (63.13%) - LeetHub

--- 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.cpp
@@ -1,24 +1,57 @@
+/*
+
+How to solve the problem
+
+# Using Sliding Window + Hashtable
+1. initialize variables
+   - hashtable with key = fruits[i] and value = total fruits[i] appears
+   - left = 0
+   - right = 0
+   - result = 0
+2. while right < fruits.size()
+   - put fruits[right] into the hastable
+   - while hashtable size > 2
+     - decrease hashtable[fruits[left]] by 1
+     - if hashtable[fruits[left]] == 0
+       remove the fruits[left] from the hashtable
+     - increase left pointer by 1
+  - get the result using this formula
+    - result = max(result, right - left + 1)
+  - increase right pointer by 1
+
+Time Complexity : O(N)
+N -> size of fruits array
+
+Memory Complexity : O(M)
+M -> total different number in fruits array
+
+*/
+
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        unordered_map<int,int> bucket;
-
-        int left, right;
-        left = right = 0;
+        unordered_map<int,int> hashTable;
+        int left = 0;
+        int right = 0;
+        int result = 0;
 
         while (right < fruits.size()) {
-            bucket[fruits[right]]++;
+            hashTable[fruits[right]]++;
 
-            if (bucket.size() > 2) {
-                bucket[fruits[left]]--;
-                if (bucket[fruits[left]] == 0) {
-                    bucket.erase(fruits[left]);
+            while (hashTable.size() > 2) {
+                hashTable[fruits[left]]--;
+
+                if (hashTable[fruits[left]] == 0) {
+                    hashTable.erase(fruits[left]);
                 }
+                
                 left++;
             }
+
+            result = max(result, (right - left) + 1);
             right++;
         }
 
-        return right - left;
+        return result;
     }
 };
