c988bb2b187cfd0949eaa6c2ea8484c239a56475
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-05 14:22:08 +0700

    Time: 0 ms (100%), Space: 36.3 MB (65.04%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,55 +1,86 @@
+/*
+
+How to solve the problem
+
+# Using two pointer
+1. find the number that closest to the x, lets called it index
+2. put left pointer = index - 1 and right pointer = index + 1
+3. just need to check
+   - if arr[right] - arr[index] < arr[left] - arr[index], then pick arr[right] and increase right pointer
+   - else pick arr[left] and increase left pointer
+   - do those steps until right - left + 1 == k
+4. loop from left to right
+   - put all of those numbers into the answer variable
+
+Time Compelxity : O(N + K)
+N -> size of arr
+
+Memory Complexity : O(1)
+
+# What makes the algorithm run slowly ? 
+1. because we need to find the number that closest to the x
+
+# How to improve the solution ? 
+1. we can change from linear search to binary search
+
+# Using two pointer + binary search
+1. find the number that closest to the x using binary search
+2. put left pointer = index - 1 and right pointer = index + 1
+3. just need to check
+   - if arr[right] - arr[index] < arr[left] - arr[index], then pick arr[right] and increase right pointer
+   - else pick arr[left] and increase left pointer
+   - do those steps until right - left + 1 == k
+4. loop from left to right
+   - put all of those numbers into the answer variable
+
+Time Compelxity : O(log N + K)
+N -> size of arr
+
+Memory Complexity : O(1)
+
+
+*/
+
+
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        
-        if(arr.size() == k){
-            return arr;
-        }
-        
-        vector<int> answer;
-        
+        vector<int> results;
+        int size = arr.size();
         int left = 0;
-        int right = arr.size() - 1;
-        int mid = 0;
-        
-        // find closed elements
-        while(left < right){
-            mid = left + (right - left) / 2;
-            
-            if(arr[mid] >= x){
-                right = mid;
-            }else if(arr[mid] < x){
+        int right = size - 1;
+
+        while (left < right) {
+            int mid = (right + left) / 2;
+
+            if (arr[mid] < x) {
                 left = mid + 1;
+            } else {
+                right = mid;
             }
         }
-        
-        right = left;
-        left = right - 1;
-        
-        // using two pointer to get the range of answers
-        while(right - left - 1 < k){
-            // cout << left << " " << right <<  endl;
-            if(left >= 0 && right < arr.size()){
-                
-                if(abs(arr[left] - x) <= abs(arr[right] - x)){
-                    left--;
-                }else{
-                    right++;
-                }
-            }else if(left < 0 && right < arr.size()){
+
+        left = left - 1;
+        right = left + 1;
+
+        while (right - left - 1 < k) {
+            if (left < 0) {
                 right++;
-            }else if(left > 0 && right >= arr.size()){
+            } else if (right >= arr.size()) {
                 left--;
+            } else if (abs(arr[left] - x) <= abs(arr[right] - x)) {
+                left--;
+            } else {
+                right++;
             }
         }
-        
-        cout << left << " " << right << endl;
-        
-        // copy the answers
-        for(int i = left + 1; i < right; i++){
-            answer.push_back(arr[i]);
+
+        left++;
+        while (left < right) {
+            results.push_back(arr[left]);
+            left++;
         }
-        
-        return answer;
+
+        return results;
     }
 };
