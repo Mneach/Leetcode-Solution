6543221cb61573a17245cbdee74d8b355993f9fd
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-06 12:59:54 +0700

    Time: 7 ms (68.05%), Space: 10.9 MB (94.56%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -1,18 +1,54 @@
+/*
+
+How to solve the problem
+
+# Using Greedy
+1. initalize variables
+   - a = -1
+   - b = -1
+   - c = -1
+   - result = 0
+2. loop from first index of string until last index of string
+   - if (s[i] == 'a') a = i
+   - else if (s[i] == 'b') b = i
+   - else if (s[i] == 'c') c = i
+   - get minimum value between (a, b, and c)
+   - if the minimum value >= 0
+     - result += (minimum value) + 1
+3. return the result
+
+Time Compelexity : O(N)
+N -> length of string s
+
+Memory Complexity : O(1)
+
+*/
+
+
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int> freq(3, 0);
-        int left = 0, total = 0;
-        
-        for (int right = 0; right < s.length(); ++right) {
-            freq[s[right] - 'a']++;
-            
-            while (freq[0] && freq[1] && freq[2]) {
-                total += s.length() - right;
-                freq[s[left++] - 'a']--;
+       int a = -1;
+       int b = -1;
+       int c = -1;
+       int result = 0;
+
+       for (int i = 0; i < s.length(); i++) {
+            if (s[i] == 'a') {
+                a = i;
+            } else if (s[i] == 'b') {
+                b = i;
+            } else if (s[i] == 'c') {
+                c = i;
             }
-        }
-        
-        return total;
+
+            int minimum = min(a, min(b, c));
+
+            if (minimum >= 0) {
+                result += minimum + 1;
+            }
+       }
+
+       return result;
     }
 };
