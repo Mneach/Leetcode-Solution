f15855e698b2fe41725d8adb4b859d8d1fb4b07b
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-06 17:04:40 +0700

    Time: 1 ms (53.73%), Space: 34 MB (96.27%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.cpp
@@ -1,29 +1,58 @@
+/*
+
+How to solve the problem
+
+# Using sliding window
+1. if k == 1, just return nums
+2. initialize variables
+   - left = 0
+   - right = 1
+   - results -> vector<int> 
+3. while right < nums.size() 
+   - if (nums[left] - nums[left - 1] != 1)
+     - move left pointer to the right pointer
+       - mark results[left] = -1
+     - if ((right - left) + 1 == k)
+       - results[left] = nums[right]
+       - increase the left pointer by 1
+     - increaes the right pointer by 1
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
-        vector<int> answer(nums.size() - (k - 1), -1);
 
-        if(nums.size() == 1) {
-            answer[0] = nums[0];
-            return answer;
-        }else if(k == 1){
+        if (k == 1) {
             return nums;
         }
 
         int left = 0;
-        
-        for(int right = 1; right < nums.size(); right++){
-            
-            if(nums[right] - nums[left] == right - left){
-                if(right - left >= k - 1){
-                    answer[left] = nums[right];
+        int right = 1;
+        vector<int> results(nums.size() - (k - 1), 0);
+
+        while (right < nums.size()) {
+            if (nums[right] - nums[right - 1] != 1) {
+                while (left < results.size() && left < right) {
+                    results[left] = -1;
                     left++;
                 }
-            }else{
-                left = right;
             }
+
+            int windowSize = (right - left) + 1;
+            if (windowSize == k) {
+                results[left] = nums[right];
+                left++;
+            }
+
+            right++;
         }
 
-        return answer;
+        return results;
     }
 };
