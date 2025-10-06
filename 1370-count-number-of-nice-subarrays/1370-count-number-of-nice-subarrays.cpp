5cd276814f90ec99d510e7199712ff7d67027fe4
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-06 16:22:49 +0700

    Time: 5 ms (73.12%), Space: 71.4 MB (67.9%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.cpp
@@ -1,32 +1,62 @@
+/*
+
+How to solve the problem
+
+# Using sliding window
+1. initialize variables
+   - left = 0
+   - right = 0
+   - count = 0
+   - result = 0
+2. while right < nums.size()
+   - if (nums[right] == 1)
+     - decrease the k by 1
+   - if k == 0
+     - reset count to 0
+   - while k == 0
+     - if nums[left] is an odd number, just increase the k value by 1
+     - increaes left pointer by 1
+     - increaes count by 1
+   - add result by count value
+   - increase right pointer by 1
+3. return the result
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int answer = 0;
-        int left = 0;
-        int tempAnswer = 0;
+        int left = 0;        
+        int right = 0;
+        int count = 0;
+        int result = 0;
 
-        int temp = k;
-        for(int i = 0; i < nums.size(); i++){
-            if(nums[i] % 2 == 1) temp--;
-
-            if(temp == 0){
-                tempAnswer++;                
-            }else if(temp < 0){
-                while(left < i && temp < 0){
-                    if(nums[left] % 2 == 1) temp++;
-                    answer += tempAnswer;
-                    left++;
-                }
-                tempAnswer = 1;
+        while (right < nums.size()) {
+            if (nums[right] % 2 == 1) {
+                k--;
             }
+
+            if (k == 0) {
+                count = 0;
+
+                while (k == 0) {
+                    if (nums[left] % 2 == 1) {
+                        k++;
+                    }
+                    left++;
+                    count++;
+                }
+            }
+
+            result += count;
+            right++;
         }
 
-        while(left < nums.size() && temp <= 0){
-            if(nums[left] % 2 == 1) temp++;
-            answer += tempAnswer;
-            left++;
-        }
-
-        return answer;
+        return result;
     }
 };
