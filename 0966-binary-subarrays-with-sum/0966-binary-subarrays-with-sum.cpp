34edc5ba931be546b7eabbd832f761dadcc2a47e
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-06 16:12:08 +0700

    Time: 0 ms (100%), Space: 32.7 MB (58.69%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,3 +1,43 @@
+/*
+
+How to solve the problem 
+
+# Using sliding window
+
+Need to split the testcases
+1. because if goal == 0, we need to count using different ways
+
+1. if goal == 0
+   - count = 0
+   - loop from the first number of nums array until last number of nums array
+     - if (nusm[i] == 0), then count++
+     - else add result by using this formula
+       - formula : result += (1LL * count * (count - 1)) / 2
+   - if count != 0
+     - add result by the formula above
+2. else
+   - left = 0
+   - right = 0
+   - count = 0
+   - while right < nums.size()
+     - decrease goal by nums[right]
+     - if goal == 0
+       - reset count = 0
+       - while goal == 0
+         - add goal by nums[left]
+         - increaes left pointer by 1
+         - increase count by 1
+     - add result by count variable
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+
+*/
+
+
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
