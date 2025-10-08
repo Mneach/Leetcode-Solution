e21817c83d2f931e50b57d6f7e247460caea92c7
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-08 12:36:29 +0700

    Time: 11 ms (87.46%), Space: 27.3 MB (90.4%) - LeetHub

--- 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.cpp
@@ -24,29 +24,53 @@ M -> total characters in the stack
 Memory Complexity : O(M)
 M -> total characters in the stack
 
+# Can we improve the memory compleixty ? 
+1. if we want to improve the memory complexity, that means we cannot use stack to solve the problem. What iam thinking is i think we can use two pointer technique to solve the problem
+
+# Using two pointer technique
+1. initialize variables
+   - left = 0
+   - right = 0
+   - result = ""
+2. while right < s.length()
+   - if (s[right] != '*')
+     - change s[l] to s[r], because the s[r] must be the valid character to take
+     - increase the left pointer
+   - else 
+     - decrease the left pointer, because we dont wakt to take the s[l] character, since there is an asterix 
+3. loop from 0 until L to get the result
+4. return the result
+
+Time Complexity : O(N + M)
+N -> length of string S
+M -> total result characters
+
+Memory Complexity : O(1)
+
 */
 
 class Solution {
 public:
     string removeStars(string s) {
         string result = "";
-        stack<char> st;
+        int left = 0;
+        int right = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (st.size() > 0 && s[i] == '*') {
-                st.pop();
-            } else if (s[i] != '*') {
-                st.push(s[i]);
+        while (right < s.length()) {
+            if (s[right] != '*') {
+                s[left] = s[right];
+                left++;
+            } else {
+                left--;
             }
+
+            right++;
         }
 
-        while (st.size() > 0) {
-            result += st.top();
-            st.pop();
+        for (int i = 0; i < left; i++) {
+            result += s[i];
         }
 
-        reverse(result.begin(), result.end());
-        
         return result;
     }
 };
