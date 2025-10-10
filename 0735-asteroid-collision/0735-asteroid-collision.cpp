370fd69cb9286e25de019319a56ba1a19c632e2d
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-10 20:40:33 +0700

    Time: 4 ms (15.6%), Space: 22.1 MB (41.47%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,42 +1,70 @@
+/*
+
+How to solve the problem 
+
+# Using stack
+1. initialize variables
+   - stack<int> st
+   - vector<int> results
+2. loop from the first index of asteroids until last index of asteroids
+   - if asteroids[i] is positive, just push the data into the stack
+   - else 
+     - while stack is not empty and stack.top() is positive 
+       - if (stack.top() > asteroids) break
+       - else if (stack.top() == asteroids)
+         - stack.pop()
+         - break
+       - else
+         - stack.pop()
+     - if stack is empty
+       - push asteroids[i] into the stack
+3. loop for every data in the stack
+   - put the number into the results array
+4. reverse the result array
+
+Time Complexity : O(N + M + N / 2)
+N -> size of asteroids array
+M -> size of stacks
+
+Memory Compelxity : O(M)
+M -> size of stacks
+
+*/
+
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        stack<int> result;
-        
-        for(int i = 0; i < asteroids.size(); i++){
-            int x = asteroids[i]; // current element of asteroids
-            
-            if(result.empty() == true) {
-                result.push(x);
-            }else{
-                int last = result.top(); // most top element in stack
-                
-                if(last > 0 && x > 0) result.push(x);
-                else if(last < 0 && x < 0) result.push(x);
-                else if(last + x == 0 && last > 0) result.pop();
-                else if(last < 0 && x > 0) result.push(x);
-                else if(last > 0 && x < 0){
-                    while(result.empty() == false && last > 0 && last + x < 0){
-                        result.pop();
-                        if(result.empty() == false) last = result.top();
-                    }
-                    
-                    if(last + x == 0) result.pop();
-                    else if(result.empty() == true) result.push(x);
-                    else if(last < 0) result.push(x); 
+        stack<int> st;
+        vector<int> results;
+
+        for (int i = 0; i < asteroids.size(); i++) {
+            bool skip = false;
+
+            while (st.size() > 0 && st.top() > 0 && asteroids[i] < 0) {
+                if (st.top() + asteroids[i] > 0) {
+                    skip = true;
+                    break;
+                } else if (st.top() + asteroids[i] < 0) {
+                    st.pop();
+                } else {
+                    skip = true;
+                    st.pop();
+                    break;
                 }
             }
+
+            if (skip == false) {
+                st.push(asteroids[i]);
+            }
         }
-        
-        vector<int> answer;
-        
-        while(result.empty() == false){
-            answer.push_back(result.top());
-            result.pop();
+
+        while (st.size() > 0) {
+            results.push_back(st.top());
+            st.pop();
         }
-        
-        reverse(answer.begin(), answer.end());
-        
-        return answer;
+
+        reverse(results.begin(), results.end());
+
+        return results;
     }
 };
