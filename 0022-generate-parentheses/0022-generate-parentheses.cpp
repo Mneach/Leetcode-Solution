87044ec4000cf1245f027b9b26bcd2a973e2190e
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-12 21:05:07 +0700

    Time: 15 ms (7.29%), Space: 27.6 MB (5.88%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,47 +1,74 @@
+/*
+
+How to solve the problem
+
+# Using recursive
+1. void recursive functions (int n, string current, open bracket, close bracket, result)
+   - if current.length() == n * 2
+     - if current is valid answer, then push current to result
+   - else if open bracket > n
+     - return
+   - else if close brakcet > open bracket
+     - return
+   - choose open bracket
+     - recursive (n, current + '(', open bracket + 1, close bracket + 1)
+   - choose close bracket
+     - recursive (n, current + '(', open bracket + 1, close bracket + 1)
+2. initialize variables
+   - vector<string> results
+3. call recrusive(n, "", 0, 0, results)
+4. return results
+
+Time Complexity : O(2^(N * 2) * (N * 2)) = O(2^N)
+N -> pairs of parentheses
+
+Memory Complexity : O(2^N * N) = O(2^N)
+N -> pairs of parentheses
+
+*/
+
 class Solution {
-public:
-    
-    void recursive(vector<string> &answer, int total, string s, int maxTotal){
-        if(maxTotal == total){
-            int totalCompleteBracket = 0;
-            int totalLeftBracket = 0;
-            int totalRightBracket = 0;
-            
-            bool valid = true;
-            
-            for(int i = 0; i < s.length(); i++){
-                if(s[i] == ')' && totalLeftBracket == 0){
-                    valid = false;
-                    break;
-                }else if(s[i] == '('){
-                    totalLeftBracket++;
-                }else if(s[i] == ')'){
-                    totalRightBracket++;
-                    
-                    if(totalLeftBracket == totalRightBracket){
-                        totalCompleteBracket += totalLeftBracket;
-                        totalLeftBracket = 0;
-                        totalRightBracket = 0;
-                    }
+private:
+    void recursive(int n, string current, int openBracket, int closeBracket, vector<string> &results) {
+        if (current.length() == n * 2) {
+            stack<char> stack;
+
+            for (int i = 0; i < current.length(); i++) {
+                if (stack.empty()) {
+                    stack.push(current[i]);
+                } else if (stack.top() == '(' && current[i] == ')') {
+                    stack.pop();
+                } else {
+                    stack.push(current[i]);
                 }
             }
-            
-            if(totalCompleteBracket == maxTotal / 2){
-                answer.push_back(s);
+
+            if (stack.empty()) {
+                results.push_back(current);
             }
-        }else{
-            string openBracket = s + "(";
-            string closeBracket = s + ")";
-            recursive(answer, total + 1, openBracket, maxTotal);
-            recursive(answer, total + 1, closeBracket, maxTotal);
+        } else if (openBracket > n) {
+            return;
+        } else if (closeBracket > openBracket) {
+            return;
         }
+
+        // choose open bracket
+        current += "(";
+        recursive(n, current, openBracket + 1, closeBracket, results);
+        current.pop_back();
+
+        // choose close bracket
+        current += ")";
+        recursive(n, current, openBracket, closeBracket + 1, results);
+        current.pop_back();
     }
-    
+
+public:
     vector<string> generateParenthesis(int n) {
-        vector<string> answer;
-        
-        recursive(answer, 0, "", n * 2);
-        
-        return answer;
+        vector<string> results;        
+
+        recursive(n, "(", 1, 0, results);
+
+        return results;
     }
 };
