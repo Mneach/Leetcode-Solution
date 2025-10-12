2ede6c42cc612f06d0f7d3cba41cbbc0642dd6a8
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-12 19:51:11 +0700

    Time: 0 ms (100%), Space: 14.1 MB (81.45%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,18 +1,87 @@
+/*
+
+How to solve the problem
+
+# Using stack 
+1. initialize class
+   - Price
+2. initialize variables
+   - stack<Price> stackPrices
+   - vector<int> results
+3. loop from the first index of prices until last index of prices
+   - curerntPrice = prices[i]
+   - priceDataFromStack;
+   - while (stackPrices.size() > 0 && stackPrices.top().getValue() >= prices[i])
+     - priceDataFromStack = stackPrices.top()
+     - stackPrices.pop()
+     - int pay = priceDataFromStack.getValue() - currentPrice
+     - int index = priceDataFromStack.getIndex()
+     - results[index] = pay
+4. while stackPrices.empty() == false
+   - priceDataFromStack = stackPrices.top()
+   - stackPrices.pop()
+   - int pay = priceDataFromStack.getPrice()
+   - int index = priceDataFromStack.getIndex(0)
+   - results[index] = pay
+   - stackPrices.pop()
+5. return results
+
+Time Complexity : O(N + M)
+N -> size of prices
+M -> size of stacks
+
+Memory Complexity : O(M)
+M -> size of stacks
+
+*/
+
+class Price {
+private: 
+    int value;
+    int index;
+public:
+    Price(int value, int index) {
+        this -> value = value;
+        this -> index = index;
+    }
+
+    int getValue() {
+        return this -> value;
+    }
+
+    int getIndex() {
+        return this -> index;
+    }
+};
+
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        vector<int> result = prices;
-
-        stack<int> stack;
+        stack<Price> stackPrices;      
+        vector<int> results(prices.size(), 0);
 
         for (int i = 0; i < prices.size(); i++) {
-            while (!stack.empty() && prices[stack.top()] >= prices[i]) {
-                result[stack.top()] -= prices[i];
-                stack.pop();
+            int currentPrice = prices[i];
+
+            while (stackPrices.size() > 0 && stackPrices.top().getValue() >= currentPrice) {
+                Price priceFromStack = stackPrices.top();
+                stackPrices.pop();
+                int pay = priceFromStack.getValue() - currentPrice;
+                int index = priceFromStack.getIndex();
+                results[index] = pay;
             }
-            stack.push(i);
+
+            stackPrices.push({currentPrice, i});
         }
 
-        return result;
+        while (stackPrices.size() > 0) {
+            Price priceFromStack = stackPrices.top();
+            stackPrices.pop();
+            int pay = priceFromStack.getValue();
+            int index = priceFromStack.getIndex();
+            results[index] = pay;
+        }
+
+        return results;
     }
 };
