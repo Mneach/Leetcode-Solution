f20452baa1ed7ebf007b0afc17db620d4a403d62
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-13 15:40:16 +0700

    Time: 0 ms (100%), Space: 10 MB (95.41%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -22,26 +22,58 @@ N -> length of string s
 Memory Complexity : O(M)
 M -> size of stack
 
+# Can we improve the solution ? 
+1. Yes we can improve the solution, Instead of using stack to maintain the result string, we can use two pointer to maintain the state
+
+# Note
+- Index 0 - left -> will be represent the data in the stack
+- s[left - 1] -> top of the stack
+- left -= 1 -> represent pop the data from stack
+- s[left] = s[right] && left += 1 -> represent push the data to stack
+
+# Using Two Pointer
+1. initialize variables
+   - left = 0
+   - right = 0
+2. while right < s.length()
+   - if (left == 0) 
+     - s[left] = s[right]
+     - left++
+   - else if (s[left - 1] == 'A' && s[right] == 'B')
+     - left--
+   - else if (s[left - 1] == 'C' && s[right] == 'D')
+     - left--
+   - else
+     - s[left] = s[right]
+     - left++
+3. return left pointer
+
+Time Complexity : O(N)
+N -> length of string s
+
+Memory Complexity : O(1)
 
 */
 
 class Solution {
 public:
     int minLength(string s) {
-        stack<char> stack;
+        int left = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            if (stack.size() == 0) {
-                stack.push(s[i]);
-            } else if (stack.top() == 'A' && s[i] == 'B') {
-                stack.pop();
-            } else if (stack.top() == 'C' && s[i] == 'D') {
-                stack.pop();
+        for (int right = 0; right < s.length(); right++) {
+            if (left == 0) {
+                s[left] = s[right];
+                left++;
+            } else if (s[left - 1] == 'A' && s[right] == 'B') {
+                left--;
+            } else if (s[left - 1] == 'C' && s[right] == 'D') {
+                left--;
             } else {
-                stack.push(s[i]);
+                s[left] = s[right];
+                left++;
             }
         }
 
-        return stack.size();
+        return left;
     }
 };
