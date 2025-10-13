b56e423cf4f27151125abeb439441c925c66b641
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-13 20:39:20 +0700

    Time: 4 ms (32.16%), Space: 12.5 MB (6.19%) - LeetHub

--- 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/1298-reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,30 +1,114 @@
+/*
+
+How to solve the problem
+
+# Using stack
+1. initialize varaibles
+   - stack<string> st
+   - word = ""
+2. loop from the first index of string s until last index of string s
+   - if s[i] == open bracket
+     - if word != ""
+       - push word into the stack
+       - word = ""
+     - push open bracket to stack
+   - else if s[i] == close bracket
+     - push word into the stack
+     - word = "";
+     - while stack.size() > 1
+       - get first data and second data from stack
+       - if second data == open bracket
+         - reverse the first data
+         - push first data into the stack
+         - break
+       - else
+         - combine second data + first data
+         - push that data into the stack
+   - else 
+     - word += s[i]
+3. if word != ""
+   - push word into the stack
+4. while stack.size() > 1
+   - if stack.size() == 1
+     - return stack.top()
+   - else
+     - get first data and second data from stack
+     - if second data == open bracket
+        - reverse the first data
+        - push first data into the stack
+        - break
+     - else
+        - combine second data + first data
+        - push that data into the stack
+4. return stack.top()
+
+Time Complexity : O(N + (M * K / 2))
+N -> length of string s
+M -> size of the stack
+K -> maximum word length in string s
+
+Memory Complexity : O(M)
+M -> size of the stack
+
+*/
+
 class Solution {
 public:
     string reverseParentheses(string s) {
-        
-        stack<int> currentLeftPosition;
-        
-        string temp = "";
-        
-        for(int i = 0; i < s.length(); i++){
-            if(s[i] >= 'a' && s[i] <= 'z') temp += s[i];
-        }
-        
-        int rightPointer = 0;
-        for(int i = 0; i < s.length(); i++){
-            
-            if(s[i] == '('){
-               currentLeftPosition.push(rightPointer); 
-            }else if(s[i] == ')'){
-                int leftPointer = currentLeftPosition.top();
-                currentLeftPosition.pop();
-                
-                reverse(temp.begin() + leftPointer, temp.begin() + rightPointer);
-            }else{
-                rightPointer++;
+        string word = "";
+        stack<string> st;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '(') {
+                if (word != "") {
+                    st.push(word);
+                    word = "";
+                }
+
+                st.push("(");
+            } else if (s[i] == ')') {
+                st.push(word);
+                word = "";
+
+                while (st.size() > 1) {
+                    string firstData = st.top(); st.pop();
+                    string secondData = st.top(); st.pop();
+
+                    if (secondData == "(") {
+                        reverse(firstData.begin(), firstData.end());
+                        st.push(firstData);
+                        break;
+                    } else {
+                        st.push(secondData + firstData);
+                    }
+                }
+            } else {
+                word += s[i];
             }
         }
-        
-        return temp;
+
+        if (word != "") {
+            st.push(word);
+        }
+
+        while (st.size() > 0) {
+
+            if (st.size() == 1) {
+                return st.top();
+            }
+
+            string firstData = st.top(); st.pop();
+            string secondData = st.top(); st.pop();
+
+            if (secondData == "(") {
+                reverse(firstData.begin(), firstData.end());
+                st.push(firstData);
+                break;
+            } else {
+                st.push(secondData + firstData);
+            }
+        }
+
+        return st.top();
     }
 };
