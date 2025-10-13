f3043ee19ff8dce5b17670688b5894ca6af0e6aa
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-13 18:44:56 +0700

    Time: 0 ms (100%), Space: 9.2 MB (24.59%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,25 +1,59 @@
+/*
+
+How to solve the problem
+
+# Using Stack
+1. initialize variables
+   - stack<char> st
+   - result = ""
+2. loop from the first idnex of string s until last index of string s
+   - if st.empty()
+     - push s[i] into the stack
+   - if st.top() is a non digit and s[i] is a digit
+     - st.pop
+   - else
+     - push s[i] into the stack
+3. loop for every ata in the stack
+   - result += st.top()
+   - st.pop()
+4. reverse the result string
+5. return result
+
+Time Complexity : O(N + M)
+N -> length of string s
+M -> size of stack
+
+Memory Compleixty : O(M)
+M -> size of stack
+
+*/
+
 class Solution {
+    bool isDigit(char x) {
+        return x >= '0' && x <= '9';
+    }
 public:
     string clearDigits(string s) {
+        string result = "";
         stack<char> st;
 
-        for (auto c : s) {
-            bool isDigit = c >= '0' && c <= '9';
-            if (st.size() > 0 && isDigit) {
+        for (int i = 0; i < s.length(); i++) {
+            if (st.empty()) {
+                st.push(s[i]);
+            } else if (isDigit(st.top()) == false && isDigit(s[i]) == true) {
                 st.pop();
-            } else if (isDigit == false) {
-                st.push(c);
+            } else {
+                st.push(s[i]);
             }
         }
 
-        string answer = "";
-        while(st.size() > 0) {
-            answer += st.top();
+        while (st.size() > 0) {
+            result += st.top();
             st.pop();
         }
 
-        reverse(answer.begin(), answer.end());
+        reverse(result.begin(), result.end());
 
-        return answer;
+        return result;
     }
 };
