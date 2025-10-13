aa1f9f618aa4eb4576fdc5ccc163b76e802aa633
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-13 15:34:30 +0700

    Time: 2 ms (55.24%), Space: 11.9 MB (7.37%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,28 +1,47 @@
+/*
+
+How to solve the problem 
+
+# Using stack
+1. initialize variables
+   - stack<char> stack
+2. loop from first index of string s until last index of string s
+   - if stack empty
+     - push s[i] into the stack
+   - else if stack.top() == 'A' && s[i] == 'B'
+     - stack.pop()
+   - else if stack.top() == 'C' && s[i] == 'D'
+     - stack.pop()
+   - else
+     - push the s[i] into the stack
+3. return stack.size()
+
+Time Complexity : O(N)
+N -> length of string s
+
+Memory Complexity : O(M)
+M -> size of stack
+
+
+*/
+
 class Solution {
 public:
     int minLength(string s) {
-        
-        string answer = s;
-        
-        
-        while(true){
-            bool check = false;    
-            
-            int length = answer.length();
-            for(int i = 0; i < length - 1; i++){
-                if((answer[i] == 'A' && answer[i + 1] == 'B') || answer[i] == 'C' && answer[i + 1] == 'D'){
-                    if(i == 0) answer = answer.substr(i + 2, answer.length());
-                    else answer = answer.substr(0 , i) + answer.substr(i + 2 , answer.length());
-                    check = true;
-                    break;
-                }else {
-                    check = false;
-                }
+        stack<char> stack;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (stack.size() == 0) {
+                stack.push(s[i]);
+            } else if (stack.top() == 'A' && s[i] == 'B') {
+                stack.pop();
+            } else if (stack.top() == 'C' && s[i] == 'D') {
+                stack.pop();
+            } else {
+                stack.push(s[i]);
             }
-            
-            if(!check) break;
         }
-        
-        return answer.length();
+
+        return stack.size();
     }
 };
