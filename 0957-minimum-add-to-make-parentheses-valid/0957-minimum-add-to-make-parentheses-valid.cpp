969d8c89831e62da7fc1ac371125b980dcca9f19
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-14 10:25:47 +0700

    Time: 0 ms (100%), Space: 8.2 MB (95.69%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -19,24 +19,61 @@ N -> length of string s
 Memory Complexity : O(M)
 M -> size of stack
 
+# Can we improve the solution ? 
+1. Yes we can, instead of using stack to maintain the state, it is better to use two pointer to maintain the state, so we can reduce the memory complexity to O(1)
+
+# Using two pointer
+1. initialize variables
+   - left = 0
+   - right = 0
+2. while right < s.length()
+   - if (left == 0 || s[right] == open bracket)
+     - change s[left] to s[right] to maintain the state
+     - left++
+   - else
+     - if (s[left - 1] == open bracket && s[right] == close bracket)
+       - left--
+     - else
+       - change s[left] to s[right] to maintain the state
+       - left++
+   - increase right pointer by 1
+3. return left pointer
+
+Time Complexity : O(N)
+N -> length of string s
+
+Memory Complexity : O(1)
+
+Note : 
+index 0 - left (exclusive) -> represent all of the data in stack
+s[left] = s[right] && left++ -> represent push the s[i] into the stack
+left == 0 -> represent stack is empty
+s[left - 1] -> get the data at top of the stack
+
 */
 
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        stack<char> st;
-        for (int i = 0; i < s.length(); i++) {
-            if (st.empty() || s[i] == '(' ) {
-                st.push(s[i]);
+        int left = 0;
+        int right = 0;
+        
+        while (right < s.length()) {
+            if (left == 0 || s[right] == '(' ) {
+                s[left] = s[right];
+                left++;
             } else {
-                if (st.top() == '(' && s[i] == ')') {
-                    st.pop();
+                if (s[left - 1] == '(' && s[right] == ')') {
+                    left--;
                 } else {
-                    st.push(s[i]);
+                    s[left] = s[right];
+                    left++;
                 }
             }
+
+            right++;
         } 
 
-        return st.size();
+        return left;
     }
 };
