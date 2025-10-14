52186d582c999ca470f8245fd74fdeb0b60ef455
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-14 18:55:46 +0700

    Time: 7 ms (55.49%), Space: 48.3 MB (77.27%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.cpp
@@ -1,51 +1,66 @@
+/*
+
+How to solve the problem
+
+Main Idea : 
+1. Need to find the next greater element from nums[i]
+
+# Using stack
+1. initialize variables
+   - stack<int> st
+   - i = 0
+   - result = 0
+2. loop for every number in nums array to construct next greater element
+   - if stack is empty
+     - push i to stack
+   - else if (stack.top() < nums[i])
+     - push i to the stack
+3. while st.size() > 0 && i < nums.size()
+   - int j = stack.top()
+   - if nums[i] > nums[j]
+     - i++
+   - else
+     - result = max(result, j - index)
+     - stack.pop()
+4. return the result
+
+Time Complexity : O(N + N) 
+N -> size of nums
+
+Memory Complexity : O(M)
+M -> size of stack
+
+*/
+
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        
-        vector<int> maxRightArr(nums.size(), 0);
-        
-        for(int i = nums.size() - 1; i >= 0; i--){
-            if(i == nums.size() - 1){
-                maxRightArr[i] = nums[i];
-                continue;
+        int i = 0;
+        int result = 0;
+        stack<int> st;
+
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (st.empty()) {
+                st.push(i);
+            } else {
+                int number = nums[st.top()];
+
+                if (number < nums[i]) {
+                    st.push(i);
+                }
             }
-            maxRightArr[i] = max(nums[i], maxRightArr[i + 1]);
         }
 
-        int left = 0;
-        int answer = 0;
-        for(int right = 1; right < nums.size(); right++){
-
-            while(nums[left] > maxRightArr[right] && left <= right){
-                left++;
+        while (st.size() > 0 && i < nums.size()) {
+            int j = st.top();
+            if (nums[i] > nums[j]) {
+                i++;
+            } else {
+                result = max(result, j - i);
+                st.pop();
             }
-
-            answer = max(right - left, answer);
         }
 
-        return answer;
+        return result;
     }
 };
-
-/*
-
-# Intuition
-- you need to know the maximum number that you can reach with the current number
-- calculate the distance between those numbers
-- to know what is the maximum number that we can reach, we can store that maximum number in an array or stacks
-- alwasy start from right to left, to its will be take o(N) to construct the maximum number that we can reach with the current number
-
-# Using two pointer approach
-1. we need to create max right array
-   1.1 start looping from the right to the left, and hold the current max value
-2. loop from 0 until nums.size() - 1
-   2.1 we need to calculate every process
-   2.2 if nums[left] > maxRightArr[current]
-   2.3 move left pointer to left + 1
-
-Time Complexity : O(N)
-O(N) -> we need to loop from left to right to construct the answer
-
-Memory Complexity : O(N)
-O(N) -> we need to use an array or stack to store the maximum number in the current state
-*/
