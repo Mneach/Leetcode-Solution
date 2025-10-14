739ded18d546b8f484a4410851a5ad438e9ebfa2
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-14 10:17:08 +0700

    Time: 0 ms (100%), Space: 8.6 MB (30.87%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -1,19 +1,42 @@
+/*
+
+# Using stack
+1. initialize variables
+   - stack<char> st
+2. loop from first index of s until last index of s
+   - if (stack.empty() || s[i] == open bracket)
+     - push s[i] into the stack
+   - else
+     - if (st.top() == open bracket && s[i] == close brakcet)
+       - st.pop()
+    - else
+       - push s[i] into the stack
+3. return stack.size()
+
+Time Complexity : O(N)
+N -> length of string s
+
+Memory Complexity : O(M)
+M -> size of stack
+
+*/
+
 class Solution {
 public:
     int minAddToMakeValid(string s) {
-        
-        stack<char> sc;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(sc.empty()){
-                sc.push(s[i]);
-            }else if(s[i] == ')' && sc.top() == '('){
-               sc.pop();
-            }else{
-                sc.push(s[i]);
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) {
+            if (st.empty() || s[i] == '(' ) {
+                st.push(s[i]);
+            } else {
+                if (st.top() == '(' && s[i] == ')') {
+                    st.pop();
+                } else {
+                    st.push(s[i]);
+                }
             }
-        }
-        
-        return sc.size();
+        } 
+
+        return st.size();
     }
 };
