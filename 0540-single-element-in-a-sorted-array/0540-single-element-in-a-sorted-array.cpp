7ecc96b595d9049e77b3bd4b794d7090607120fd
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-15 14:55:25 +0700

    Time: 1 ms (5.46%), Space: 26.1 MB (91.96%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,47 +1,85 @@
+
+/*
+
+How we can solve the problem
+
+# Using brute force solution
+1. loop from first index until last index of nums
+   - if nums[i] != nums[i - 1] && nums[i] != nums[i + 1]
+     - return nums[i]
+2. return -1
+
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Compelxity : O(1)
+
+# Can we improve the solution
+1. Yes we can, instead of using linear search to find the answer, we can use binary search and recursive to find the answer
+
+# Using binary search + recursive
+1. basecase
+   - if (nums.size() == 1)
+     - return nums[0]
+2. recursive functions
+   - basecase
+     - if left > right, return -1
+   - int mid = left + (right - left) / 2
+   if nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]
+     - return nums[mid]
+   - else
+     - leftPart = recursive(left, mid - 1, nums)
+     - if leftPart != -1 return leftPart
+     - rightPart = recursive(mid + 1, right, nums)
+     - if rightPart != -1 return rightPart
+     - return -1
+3. call recursive function
+
+Time Complexity : O(log N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
+private:
+    int findSingleElement(int left, int right, vector<int>& nums) {
+        if (left > right) {
+            return -1;
+        }
+
+        int mid = left + (right - left) / 2;
+        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+            return nums[mid];
+        } else {
+            int leftPart = findSingleElement(left, mid - 1, nums);
+            if (leftPart != -1) {
+                return leftPart;
+            }
+
+            int rightPart = findSingleElement(mid + 1, right, nums);
+            return rightPart;
+        }
+    }
 public:
-    
-    int binarySearch(vector<int> &nums, int left, int right){
-        while(left <= right){
-            int mid = left + (right - left) / 2;
-            if(mid == 0){
-                if(nums[mid] != nums[mid + 1]){
-                    return nums[mid];
-                }else{
-                    return -1;
-                }
-            }else if(mid == nums.size() - 1){
-                if(nums[mid] != nums[mid - 1]){
-                    return nums[mid];
-                }else{
-                    return -1;
-                }
-            }else{
-                
-                if(nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]){
-                    return nums[mid];
-                }
-                
-                int leftResult = binarySearch(nums, left, mid - 1);
-                int rightResult = binarySearch(nums, mid + 1, right);
-                
-                if(leftResult != -1) return leftResult;
-                if(rightResult != -1) return rightResult;
-                
-                break;
+    int singleNonDuplicate(vector<int>& nums) {
+
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+
+        int result = findSingleElement(1, nums.size() - 2, nums);
+        
+        if (result == -1) {
+
+            if (nums[0] != nums[1]) {
+                result = nums[0];
+            } else {
+                result = nums[nums.size() - 1];
             }
         }
-        
-        return -1;
-    }
-    
-    int singleNonDuplicate(vector<int>& nums) {
-        
-        if(nums.size() == 1) return nums[0];
-        
-        int left = 0;
-        int right = nums.size() - 1;
-        
-        return binarySearch(nums, left, right);
+
+        return result;
     }
 };
