605b466d96a7d700d59bf38f2447969f6560199a
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-15 22:09:04 +0700

    Time: 0 ms (100%), Space: 12.7 MB (33.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,55 +1,88 @@
+/*
+
+Rules : 
+1. peak number = nums[i] > nums[i - 1] && nums[i] > nums[i + 1]
+2. i == 0, then nums[i - 1] = -infinity
+3. i == nums.size(), then nums[i + 1] = -infinity
+
+# Using brute force 
+
+1. loop from the first index of array until last index of array
+   - if the current index is a peak number
+     - return current index
+
+Time Complexity : O(N)
+N -> size of nums array
+
+Memory Complexity : O(1)
+
+# Can we improve the solution
+1. Yes we can, instead of using linear search to find a pick number, we can use binary search to find a pick number. The main idea is, we just need to find the higest number in the current sub array (nums[L] until nums[R]), so need to follow these rules
+   - if nums[i] < nums[i + 1], then move to the right part. Because we know that the higest number should be in the right part of the i index
+   - if nums[i] < nums[i - 1], then move to the left part. Because we know that the highest number should be in the left part of the i index
+
+# Using Binary Search
+1. Initialize variables
+   - left = 0
+   - right = nums.size() - 1
+2. while left <= right (binary sesarch algorithm)
+   - mid = left + (right - left) / 2
+   - currentNumber = nums[mid]
+   - prevNumber
+   - nextNumber
+   - if (mid == 0), then prevNumber = INT_MIN
+   - else, then prevNumber = nums[mid - 1]
+   - if (mid == nums.size() - 1), then nextNumber = INT_MIN
+   - else, then nextNumber = nums[mid + 1]
+   - if currentNumber is a peak number
+     - return mid
+   - else if (currentNumber < nextNumber)
+     - left = mid + 1
+   - else
+     - right = mid - 1
+3. return -1
+
+Time Complexity : O(log N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
+
 class Solution {
 public:
-    
-    int binarySearch(vector<int>& nums, int left, int right){
-        bool recursiveCheck = false;
-        if(left <= right){
-            int mid = (left + right) / 2;
-            if(mid == 0){
-               // 10 1
-               if(nums[mid] > nums[mid + 1]) return mid;   
-               recursiveCheck = true;
-            }else if(mid == nums.size() - 1){
-               if(nums[mid] > nums[mid - 1]) return mid;
-               recursiveCheck = true;
-            }else if(nums[mid] >= nums[mid - 1] && nums[mid] >= nums[mid + 1]){
-                // 1 2 1
-                return mid;
-            }else if(nums[mid] <= nums[mid - 1] && nums[mid] <= nums[mid + 1]){
-                // 2 1 2
-                recursiveCheck = true;
-            }else if(nums[mid] <= nums[mid - 1] && nums[mid] >= nums[mid + 1]){
-                // 3 2 1
-                recursiveCheck = true;
-            }else if(nums[mid] >= nums[mid - 1] && nums[mid] <= nums[mid + 1]){
-                // 1 2 3
-                recursiveCheck = true;
+    int findPeakElement(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int currentNumber = nums[mid];
+            long long prevNumber;
+
+            if (mid == 0) {
+                prevNumber = LLONG_MIN;
+            } else {
+                prevNumber = nums[mid - 1];
             }
-            
-            if(recursiveCheck){
-                recursiveCheck = false;
-                // recursive right
-                int recursiveRight = binarySearch(nums, mid + 1 , right);
-                // recursive left
-                int recursiveLeft = binarySearch(nums, left, mid - 1);
-                
-                if(recursiveRight != -1){
-                    return recursiveRight;
-                }else if(recursiveLeft != -1){
-                    return recursiveLeft;
-                }else{
-                    return -1;
-                }
+
+            long long nextNumber;
+            if (mid == nums.size() - 1) {
+                nextNumber = LLONG_MIN;
+            } else {
+                nextNumber = nums[mid + 1];
+            }
+
+            if (currentNumber > prevNumber && currentNumber > nextNumber) {
+                return mid;
+            } else if (currentNumber < nextNumber) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        
+
         return -1;
-    }
-    
-    int findPeakElement(vector<int>& nums) {
-       if(nums.size() == 1) return 0;
-       if(nums.size() == 2) return nums[0] > nums[1] ? 0 : 1;
-        
-       return binarySearch(nums, 0, nums.size() - 1);
     }
 };
