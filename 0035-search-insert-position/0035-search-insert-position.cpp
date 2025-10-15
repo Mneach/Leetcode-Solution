8d5da2b0750f03256667c1c9f208b866f4c6c6e0
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-15 19:49:46 +0700

    Time: 0 ms (100%), Space: 13.7 MB (42.01%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,23 +1,52 @@
+
+/*
+
+How to solve the problem
+1. initialize variables
+   - left = 0
+   - right = nums.size() - 1
+   - result = 0
+2. do binary search algorithm
+   - while left <= right
+     - mid = left + (right - left) / 2
+     - if (nums[mid] == target)
+       - result = mid
+       - break
+     - else if (nums[mid] < target) 
+       - left = mid + 1
+       - result = left
+     - else 
+       - right = mid - 1
+3. return result
+
+Time Complexity : O(log N)
+N -> size of nums
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        
         int left = 0;
         int right = nums.size() - 1;
-        int mid = 0;
-        
-        while(left <= right){
-            mid = (left + right) / 2;
-            
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] < target){
+        int result = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                result = mid;
+                break;
+            } else if (nums[mid] < target) {
                 left = mid + 1;
-            }else{
+                result = left;
+            } else {
                 right = mid - 1;
             }
         }
-        
-        return nums[mid] < target ? mid + 1 : mid;
+
+        return result;
     }
 };
