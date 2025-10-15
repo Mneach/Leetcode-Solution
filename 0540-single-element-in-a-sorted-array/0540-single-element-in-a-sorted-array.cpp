5bfda64a95d5f3fbb5c1d676c75ebd47e010b300
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-15 16:06:56 +0700

    Time: 0 ms (100%), Space: 26.3 MB (33.86%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -35,6 +35,30 @@ Memory Compelxity : O(1)
      - return -1
 3. call recursive function
 
+Time Complexity : O(N)
+N -> size of nums
+
+Memory Complexity : O(N)
+
+# Can we improve the solution again
+1. Yes we can, the idea if the current mid is not our answer, we need to move either to the left or to the right
+2. since we know that all of the numbers appear twice, except the answer, by default
+   - if mid == even index, than means nums[mid] == nums[mid + 1]
+   - if mid == odd index, that means nums[mid] == nums[mid - 1]
+3. if the current condition is not full fill that rules, we need to move the left or right pointer
+   - if mid == even index && nums[mid] != nums[mid + 1], that means the answer should be in the left of the current mid
+     - so move the right pointer to the mid - 1
+   - else
+     - move the left pointer to the mid + 1
+
+# Using binary search
+1. initialize variables
+   - left = 0
+   - right = nums.size() - 1
+   - result = 0
+2. we will start binary search from index 1 until nums.size() - 1 
+3. return result
+
 Time Complexity : O(log N)
 N -> size of nums
 
@@ -43,43 +67,28 @@ Memory Complexity : O(1)
 */
 
 class Solution {
-private:
-    int findSingleElement(int left, int right, vector<int>& nums) {
-        if (left > right) {
-            return -1;
-        }
-
-        int mid = left + (right - left) / 2;
-        if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
-            return nums[mid];
-        } else {
-            int leftPart = findSingleElement(left, mid - 1, nums);
-            if (leftPart != -1) {
-                return leftPart;
-            }
-
-            int rightPart = findSingleElement(mid + 1, right, nums);
-            return rightPart;
-        }
-    }
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int result = 0;
 
-        if (nums.size() == 1) {
-            return nums[0];
-        }
+        while (left < right) {
+            int mid = left + (right - left) / 2;
 
-        int result = findSingleElement(1, nums.size() - 2, nums);
-        
-        if (result == -1) {
-
-            if (nums[0] != nums[1]) {
-                result = nums[0];
+            if (mid % 2 == 1) {
+                // to make mid = even index
+                mid--;
+            }
+            
+            if (nums[mid] != nums[mid + 1]) {
+                right = mid;
             } else {
-                result = nums[nums.size() - 1];
+                left = mid + 2;
             }
         }
+        
 
-        return result;
+        return nums[left];
     }
 };
