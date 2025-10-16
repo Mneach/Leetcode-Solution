141589d99590264061b57050703288baafb5cc93
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-16 15:02:02 +0700

    Time: 33 ms (28.69%), Space: 88.3 MB (32.6%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.cpp
@@ -1,3 +1,61 @@
+/*
+
+How we can solve the problem
+
+# Using Brute force solution
+1. initialize variables
+   - minimumCandy = 1
+   - maximumCandy = get maximum candy in candies array
+   - result = 0
+2. loop from the minimumCandy until maximumCandy
+   - check if the currentCandy can be distributed to the k childs
+     - totalChild = 0
+     - valid = false
+     - loop from every candy in candies array
+       - totalChild += candy / currentCandy
+       - if totalChild >= k
+         - valid = true
+         - break the loop 
+     - if totalChild >= k
+       - mark current candy as our answer (result = max(result, currentCandy))
+3. return the result
+
+Time Complexity : O(N * M)
+N -> size of candies array
+M -> maximum candy in candies array
+
+Memory Complexity : O(1)
+
+# Using binary search
+1. initialize variables
+   - minimumCandy = 1
+   - maximumCandy = get maximum candy in candies array
+   - result = 0
+2. while minimumCandy <= maximumCandy (do binary search algorithm)
+   - currentCandy = minimumCandy + (maximumCandy - minimumCandy) / 2
+   - check if the currentCandy can be distributed to the k childs
+     - totalChild = 0
+     - valid = false
+     - loop from every candy in candies array
+       - totalChild += candy / currentCandy
+       - if totalChild >= k
+         - valid = true
+         - break the loop 
+     - if totalChild >= k
+       - mark current candy as our answer (result = max(result, currentCandy))
+       - minimumCandy = currentCandy + 1
+     - else
+       - maximumCandy = currentCandy - 1
+3. return the result
+
+Time Complexity : O(N * log M)
+N -> size of array
+M -> maximum candies in candy array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int maximumCandies(vector<int>& candies, long long k) {
