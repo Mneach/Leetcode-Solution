fbda8cbb17b0206b7a82e7fd3d8d047c2e150936
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-17 07:25:48 +0700

    Time: 20 ms (88.37%), Space: 86.7 MB (48.26%) - LeetHub

--- 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     int minimizeMax(vector<int>& nums, int p) {
         sort(nums.begin(), nums.end());
-        int left = 0;
-        int right = nums.back() - nums.front();
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (countValidPairs(nums, mid) >= p) {
-                right = mid;
-            } else {
-                left = mid + 1;
-            }
-        }
-        
-        return left;
-    }
 
-private:
-    // Helper function to count valid pairs with difference <= threshold
-    int countValidPairs(const vector<int>& nums, int threshold) {
-        int count = 0;
-        
-        for (int i = 0; i < nums.size() - 1; ) {
-            if (nums[i + 1] - nums[i] <= threshold) {
-                count++;
-                i += 2;  // Skip the next element as it's part of the current pair
+        int minimumX = 0;
+        int maximumX = nums[nums.size() - 1];
+        int result = INT_MAX;
+
+        while (minimumX <= maximumX) {
+            int x = minimumX + (maximumX - minimumX) / 2;
+
+            // check if x is a valid result
+            int totalPairs = 0;
+            for (int i = 0; i < nums.size() - 1; i++) {
+                if (nums[i + 1] - nums[i] <= x) {
+                    totalPairs++;
+                    i++;
+                }
+            }
+
+            if (totalPairs < p) {
+                minimumX = x + 1;
             } else {
-                i++;
+                result=  x;
+                maximumX = x - 1;
             }
         }
-        
-        return count;
+
+        return result;
     }
 };
