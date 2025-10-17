b162d261a08689d1db8283803619467053c82f4d
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-17 12:08:07 +0700

    Time: 34 ms (49.1%), Space: 59.9 MB (11.95%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,3 +1,103 @@
+
+/*
+
+# Observation
+1.	we need to perform at most maxOperations times
+2.	Take any bag of balls and divide it into two new bags with a positive number of balls.
+3.	We need to minimize the penalty after the operations
+4.	After do all of the operations, size of the array should become equal to the 2x from original size
+
+# Brute Force Solution
+
+
+# Simulation
+
+minPenalty = 1
+maxPenalty = 8
+
+       I
+[2,4,8,2], maxOperations = 4
+
+penalty = 2
+
+totalOperations = (nums[i] / penalty) - 1
+totalOperations = 4
+
+[1,1,1,1,1,1]
+
+# Pseodecode
+1.	initialize variables
+   - minPenalty = 1
+   - maxPenalty = maximum number in nums array
+   - result = maxPenalty
+2. loop from minPenalty until maxPenalty
+   - check if penalty is a valid result
+     - totalOperations = 0 
+     - loop for every number in nums array
+       - if (nums[i] % penalty) == 0
+         - totalOperations += (nums[i] / penalty) - 1
+       - else
+         - totalOperations += (nums[i] / penalty)
+     - if totalOperations == maxOperations
+       - result = penalty
+       - break
+3. return the result
+
+Time complexity : O(N * M)
+N -> size of nums array
+M -> max penalty value -> max number in nums array
+
+Memory Complexity : O(1)
+
+# Binary search
+
+# Simulation
+nums = [2,4,8,2], maxOperations = 4
+
+minPenalty = 2
+maxPenalty = 1
+penalty = minPenalty + (maxPenalty - minPenalty) / 2
+penalty = 1
+
+result = 2
+
+ I
+[2,4,8,4]
+
+if (nums[i] <= penalty) skip nums[i]
+
+totalOperations = (nums[i] / penalty) - 1
+totalOperations = 1 + 3 + 7 + 3 = 14
+
+# Pseodecode
+1.	initialize variables
+   - minPenalty = 1
+   - maxPenalty = maximum number in nums array
+   - result = maxPenalty
+2. while minPenalty <= maxPenalty (do binary search algorithm)
+   - penalty = minPenalty + (maxPenalty - minPenalty) / 2
+   - check if penalty is a valid result
+     - totalOperations = 0
+     - loop for every number in nums array
+       - if (nums[i] % penalty) == 0
+         - totalOperations += (nums[i] / penalty) - 1
+       - else
+         - totalOperations += (nums[i] / penalty)
+   - if totalOperations <= maxOperations
+     - result = penalty
+     - maxPenalty = penalty - 1
+   - else
+     - minPenalty = penalty + 1
+3. return the result
+
+Time Complexity : O(N * log M)
+N -> size of nums
+M -> max number in nums array
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
 public:
     int minimumSize(vector<int>& nums, int maxOperations) {
@@ -7,8 +107,6 @@ public:
 
         while (minPenalty <= maxPenalty) {
             int penalty = minPenalty + (maxPenalty - minPenalty) / 2;
-
-            cout << penalty << endl;
 
             // check if current penalty is a valid answer
             long long totalOperations = 0;
