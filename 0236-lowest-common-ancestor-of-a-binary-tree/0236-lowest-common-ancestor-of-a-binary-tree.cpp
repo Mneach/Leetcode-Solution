0791e610cda6858e8020cc667b293e8e4d4dccfd
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-21 16:45:00 +0700

    Time: 8 ms (88.92%), Space: 17.4 MB (40.62%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -7,40 +7,57 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+/*
+
+# Observation
+1.	we need to visit the child first, then the current node
+2. if the current node is not equal to p or q, just return NULL
+3. otherwise return the current node, because the current code might be our answer
+4. if left child and right child is not equal to NULL, we should return the parent
+
+# Postorder Tranversal
+
+# Simulation
+p = 5
+q = 1
+
+           3
+          / \
+         5   1
+        /\   /\
+       6  2  0 8
+         /\
+        7  4
+
+Time Complexity : O(N)
+N -> total nodes
+
+Memory Complexity : O(1)
+
+*/
+
 class Solution {
-private:
-    int total = 0;
-    TreeNode* result;
-
-    int dfs(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL) {
-            return 0;
-        }
-
-        int resultLeft = dfs(root -> left, p, q);
-        int resultRight = dfs(root -> right, p , q);
-
-        if (result != NULL) {
-            return 0;
-        } else if (resultLeft + resultRight == 2) {
-            result = root;
-        }
-
-        if (root -> val == p -> val || root -> val == q -> val) {
-
-            if (resultLeft == 1 || resultRight == 1) {
-                result = root;
-            }
-
-            return 1;
-        } else {
-            return max(resultLeft, resultRight);
-        }
-    }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        dfs(root, p, q);
+        if (root == NULL) {
+            return root;
+        } else if (root == p) {
+            return root;
+        } else if (root == q) {
+            return root;
+        }
 
-        return result;
+        // do post order traversal
+        TreeNode* left = lowestCommonAncestor(root -> left, p, q);
+        TreeNode* right = lowestCommonAncestor(root -> right, p, q);
+
+        if (left != NULL && right != NULL) {
+            return root;
+        } else if (left != NULL) {
+            return left;
+        } else {
+            return right;
+        }
     }
 };
