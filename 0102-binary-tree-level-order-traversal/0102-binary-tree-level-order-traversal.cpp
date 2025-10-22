99b466889e67a3f7359749a2be41dbcc291cd530
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-22 15:06:39 +0700

    Time: 0 ms (100%), Space: 17 MB (88.38%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -9,6 +9,55 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+/*
+
+# Observation
+1.	We need to traverse the tree level by level
+
+# BFS (Queue)
+
+# Simulation
+
+    3
+   / \
+  9  20
+     /\
+    15 7
+
+
+queue = 
+
+resultPerLevel = [15,7]
+
+results = [[3],[9,20],[15,7]]
+
+# Pseudocode
+1.	initialize variable
+   - queue<TreeNode*> q
+   - vector<vector<int>> results
+2. if root == NULL, return results
+3. push root into the queue
+4. while queue is not empty
+   - vector<int> result
+   - get the queue size
+   - loop from index 1 until size
+     - get the node from the queue
+     - pop the node from the queue
+     - if the node has left child, push the left child into the queue
+     - if the node has right child, push the right child into the queue
+     - push the node into the result
+   - push result into results array
+5. return the results array
+
+Time Complexity : O(N)
+N -> total nodes in the tree
+
+Memory Complexity : O(P)
+P -> maximum total node at level x
+
+*/
+
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
