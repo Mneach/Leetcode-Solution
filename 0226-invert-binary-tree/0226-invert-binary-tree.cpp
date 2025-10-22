11ebe705db6ce7c648d64a4afbc57c0d57ba6012
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-22 10:58:15 +0700

    Time: 0 ms (100%), Space: 12.4 MB (59.31%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -10,26 +10,54 @@
  * };
  */
 
+/*
+
+# Observation
+1.	We need to visit the child first, so we will use post order traversal to traverse the tree
+   - visit left child
+   - visit right child
+   - swap left and right child
+   - return root
+
+# DFS (post order traversal)
+
+# Simulation
+
+     4
+   /  \
+  2    7
+ / \   /\
+3   1 9  6
+
+# Pseudocode
+1.	create invertTree function
+   - if root == NULL, return root
+   - visit left child
+   - visit right chlid
+   - swap the current left child with current right child
+   - return root
+
+Time Complexity : O(N)
+N -> total node in the binary tree
+
+Memory Complexity : O(H)
+H -> height of binary tree
+
+*/
+
+
 class Solution {
 public:
-
-    void invertRoot(TreeNode* root){
-        if(root == NULL) return;
-        
-        TreeNode* temp = root->left;
-        root->left = root->right;
-        root->right = temp;
-        
-        invertRoot(root->left);
-        invertRoot(root->right);
-    }
-
     TreeNode* invertTree(TreeNode* root) {
-        
-        if(root == NULL) return root;
-        else {
-            invertRoot(root);
+        if (root == NULL) {
+            return root;
         }
+
+        TreeNode* leftChild = invertTree(root -> left);
+        TreeNode* rightChild = invertTree(root -> right);
+
+        root -> left = rightChild;
+        root -> right = leftChild;
 
         return root;
     }
