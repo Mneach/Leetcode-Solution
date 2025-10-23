b112a682bda70a5bf1a7f37e0f7933928674a2bc
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-23 17:47:40 +0700

    Time: 202 ms (18.46%), Space: 217.2 MB (29.08%) - LeetHub

--- 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -13,44 +13,54 @@ class Solution {
 public:
     int minimumOperations(TreeNode* root) {
         queue<TreeNode*> q;
+        int totalOperation = 0;
+
+        if (root == NULL) {
+            return totalOperation;
+        }
+
         q.push(root);
+        while (q.empty() == false) {
+            vector<int> childValues;
+            unordered_map<int,int> bucket;
+            int size = q.size();
 
-        int answer = 0;
-
-        while (q.size() > 0){
-            int totalNode = q.size();
-            vector<int> idx(totalNode, 0);
-            // fill with inc value
-            iota(idx.begin(), idx.end(), 0);
-
-            vector<int> arr(totalNode, 0); 
-
-            for (int i = 0; i < totalNode; i++) {
-                auto node = q.front();
+            for (int i = 0; i < size; i++) {
+                TreeNode* currentNode = q.front();
                 q.pop();
 
-                arr[i] = node -> val;
-                if (node -> left) q.push(node -> left);
-                if (node -> right) q.push(node -> right);
+                if (currentNode -> left) {
+                    childValues.push_back(currentNode -> left -> val);
+                    bucket[currentNode -> left -> val] = childValues.size() - 1;
+                    q.push(currentNode -> left);
+                }
+
+                if (currentNode -> right) {
+                    childValues.push_back(currentNode -> right -> val);
+                    bucket[currentNode -> right -> val] = childValues.size() - 1;
+                    q.push(currentNode -> right);
+                }
             }
 
-            // sort the index
-            sort (idx.begin(), idx.end(), [&](int left, int right){
-                return arr[left] < arr[right];
-            });
+            vector<int> sortedChildValues = childValues;
+            sort(sortedChildValues.begin(), sortedChildValues.end());
 
-            // compare
-            for (int i = 0; i < totalNode;) {
-                int j = idx[i];
+            for (int i = 0; i < childValues.size(); i++) {
 
-                if (j != i) {
-                    answer++;
-                    swap(idx[i], idx[j]);
-                } else {
-                    i++;
+                if (childValues[i] != sortedChildValues[i]) {
+
+                    totalOperation += 1;
+
+                    int correctIndex = bucket[sortedChildValues[i]];
+                    // update the index in the bucket
+                    bucket[childValues[i]] = correctIndex;
+                    
+                    // swap the value
+                    swap(childValues[i], childValues[correctIndex]);
                 }
             }
         }
-        return answer;
+
+        return totalOperation;
     }
 };
