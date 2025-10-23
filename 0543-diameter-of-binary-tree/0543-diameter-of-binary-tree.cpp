a8c498f0f239743e9495f3eed744680d84f79dd8
Fitra yuda <90248869+fitra-yuda@users.noreply.github.com>
2025-10-23 14:06:29 +0700

    Time: 0 ms (100%), Space: 24 MB (13.99%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -1,25 +1,48 @@
-#include <algorithm>
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 private:
-    int diameter;
 
+    int result = 0;
+
+    int dfs(TreeNode* root) {
+        if (root == NULL) {
+            return 0;
+        } else if (root -> left == NULL && root -> right == NULL) {
+            return 0;
+        }
+
+        int leftResult = dfs(root -> left);
+        int rightResult = dfs(root -> right);
+
+        // calculate the current result
+        int currentResult = leftResult + rightResult;
+
+        if (root -> left) {
+            currentResult += 1;
+        }
+
+        if (root -> right) {
+            currentResult += 1;
+        }
+
+        result = max(result, currentResult);
+
+        return max(leftResult, rightResult) + 1;
+    }
 public:
     int diameterOfBinaryTree(TreeNode* root) {
-        diameter = 0;
-        longestPath(root);
-        return diameter;
-    }
+        dfs(root);
 
-private:
-    int longestPath(TreeNode* node) {
-        if (node == nullptr) return 0;
-
-        int leftPath = longestPath(node->left);
-        int rightPath = longestPath(node->right);
-
-        diameter = std::max(diameter, leftPath + rightPath);
-
-        return std::max(leftPath, rightPath) + 1;
+        return result;
     }
 };
